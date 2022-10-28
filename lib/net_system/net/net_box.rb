212cb9d9ca2e78a9f40b6f71c5a6302eb24eb5c1
Thiago Pinto <thiagopintodev@gmail.com>
2022-10-28 00:07:37 -0300

    NetSystem: Removing deprecation warning

--- lib/net_system/net/net_box.rb
+++ lib/net_system/net/net_box.rb
@@ -3,14 +3,6 @@ class NetSystem
     has_panel :client, :clients
     has_controller :client, :clients
 
-    begin
-      raise "remove the deprecation warning below" unless ["1.0.0", "1.1.0"].include? Lizarb::VERSION
-
-      def self.adapters
-        raise "please rename this method to `clients`"
-      end
-    end
-
     # Set up your client panel per the DSL in http://guides.lizarb.org/panels/client.html
     clients do
       # set :log_level, ENV["net.client.log_level"]
