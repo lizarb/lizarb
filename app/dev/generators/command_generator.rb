6e0f4d99a494cd28ee1c1868e366006acda99e8e
Thiago Pinto <thiagopintodev@gmail.com>
2023-01-15 15:02:47 -0300

    CommandGenerator now renders with ERB

--- app/dev/generators/command_generator.rb
+++ app/dev/generators/command_generator.rb
@@ -6,46 +6,13 @@ class CommandGenerator < Liza::Generator
   generate :controller do
     folder FOLDER
     filename "#{name}_command.rb"
-    content command_content
+    content render "command.rb"
   end
 
   generate :controller_test do
     folder FOLDER
     filename "#{name}_command_test.rb"
-    content command_test_content
-  end
-
-  # helper methods
-
-  def command_content
-    <<~CODE
-class #{name.camelize}Command < Liza::Command
-
-  def self.call args
-    log :higher, "Called \#{self}.\#{__method__} with args \#{args}"
-
-    #
-  end
-
-end
-    CODE
-  end
-
-  def command_test_content
-    <<~CODE
-class #{name.camelize}CommandTest < Liza::CommandTest
-
-  test :subject_class do
-    assert subject_class == #{name.camelize}Command
-  end
-
-  test :settings do
-    assert subject_class.log_level == :normal
-    assert subject_class.log_color == :green
-  end
-
-end
-    CODE
+    content render "command_test.rb"
   end
 
 end
