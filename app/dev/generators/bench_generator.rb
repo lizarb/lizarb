7032d5b58c6ddf76df36b61da45a74acd8bf2a5e
Thiago Almeida <thiago@lizarb.org>
2023-01-15 15:04:23 -0300

    BenchGenerator now renders with ERB

--- app/dev/generators/bench_generator.rb
+++ app/dev/generators/bench_generator.rb
@@ -6,62 +6,13 @@ class BenchGenerator < Liza::Generator
   generate :controller do
     folder FOLDER
     filename "#{name}_bench.rb"
-    content bench_content
+    content render "bench.rb"
   end
 
   generate :controller_test do
     folder FOLDER
     filename "#{name}_bench_test.rb"
-    content bench_test_content
-  end
-
-  # helper methods
-
-  def bench_content
-    <<~CODE
-class #{name.camelize}Bench < AppBench
-
-  main_dsl
-
-  setup do
-    N = 1_000_000
-  end
-
-  mark "alternative 1" do
-    i = 0
-
-    while (i += 1) <= N
-      a = "1".to_sym
-    end
-  end
-
-  mark "alternative 2" do
-    i = 0
-
-    while (i += 1) <= N
-      a = :"1"
-    end
-  end
-
-end
-    CODE
-  end
-
-  def bench_test_content
-    <<~CODE
-class #{name.camelize}BenchTest < Liza::BenchTest
-
-  test :subject_class do
-    assert subject_class == #{name.camelize}Bench
-  end
-
-  test :settings do
-    assert subject_class.log_level == :normal
-    assert subject_class.log_color == :green
-  end
-
-end
-    CODE
+    content render "bench_test.rb"
   end
 
 end
