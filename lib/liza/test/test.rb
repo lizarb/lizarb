ade7fcbbe02ef6d14d062151c2006568111d9d6d
Thiago Pinto <thiagopintodev@gmail.com>
2023-02-13 21:03:39 -0300

    Test. Filters run and are passed down to sub-classes and sub-groups. assert_equality and assert_raises fix #60 fix #61

--- lib/liza/test/test.rb
+++ lib/liza/test/test.rb
@@ -2,9 +2,11 @@ module Liza
   class Test < Unit
 
     part :test_assertions
+    part :test_assertions_advanced
     part :test_dsl
-    part :test_context
+    part :test_log
     part :test_subject
+    part :test_tree
 
   end
 end
---
+++ lib/liza/test_parts/test_assertions_advanced_part.rb
@@ -0,0 +1,127 @@
+module Liza
+  class TestAssertionsAdvancedPart < Part
+    insertion do
+      
+      def assert_equality a, b, msg = nil, kaller: nil
+        ret = a == b
+
+        if ret
+          self.class._assertion_passed
+          @last_result = :passed
+        else
+          self.class._assertion_failed msg
+          @last_result = :failed
+        end
+
+        kaller ||= caller
+        log_test_assertion __method__, kaller if _groups.empty?
+
+        if log_test_assertion_message?
+          msg ||= "#{__method__} #{a}, #{b} (== equality)"
+          log_test_assertion_message ret, msg
+        end
+
+        ret
+      end
+
+      def refute_equality a, b, msg = nil, kaller: nil
+        ret = a == b
+
+        if ret
+          self.class._assertion_failed msg
+          @last_result = :failed
+        else
+          self.class._assertion_passed
+          @last_result = :passed
+        end
+
+        kaller ||= caller
+        log_test_assertion __method__, kaller if _groups.empty?
+
+        if log_test_assertion_message?
+          msg ||= "#{__method__} #{a}, #{b} (== equality)"
+          log_test_assertion_message !ret, msg
+        end
+
+        ret
+      end
+
+      def assert_equality! a, b, msg = nil
+        critical assert_equality a, b, msg, kaller: caller
+      end
+
+      def refute_equality! a, b, msg = nil
+        critical refute_equality a, b, msg, kaller: caller
+      end
+
+      def assert_raises exception_klass, msg = nil, kaller: nil, &block
+        raise ArgumentError, "No block given" unless block_given?
+
+        ret = false
+        begin
+          yield
+        rescue => e
+          ret = exception_klass === e
+          error = e
+        end
+
+        if ret
+          self.class._assertion_passed
+          @last_result = :passed
+        else
+          self.class._assertion_failed msg
+          @last_result = :failed
+        end
+
+        kaller ||= caller
+        log_test_assertion __method__, kaller if _groups.empty?
+
+        if log_test_assertion_message?
+          msg ||= "#{__method__} (#{exception_klass}) #{error.inspect}"
+          log_test_assertion_message ret, msg
+        end
+
+        ret
+      end
+
+      def refute_raises exception_klass, msg = nil, kaller: nil, &block
+        raise ArgumentError, "No block given" unless block_given?
+
+        ret = false
+        begin
+          yield
+        rescue => e
+          ret = !exception_klass === e
+          error = e
+        end
+
+        if ret
+          self.class._assertion_failed msg
+          @last_result = :failed
+        else
+          self.class._assertion_passed
+          @last_result = :passed
+        end
+
+        kaller ||= caller
+        log_test_assertion __method__, kaller if _groups.empty?
+
+        if log_test_assertion_message?
+          msg ||= "#{__method__} (#{exception_klass}) #{error.inspect}"
+          log_test_assertion_message !ret, msg
+        end
+
+        ret
+      end
+
+      def assert_raises! e, msg = nil
+        critical assert_raises e, msg, kaller: caller
+      end
+
+      def refute_raises! e, msg = nil
+        critical refute_raises e, msg, kaller: caller
+      end
+
+    end
+  end
+end
