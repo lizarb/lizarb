f42944caffe94cf5a95fccd7688a4f16ac341c4c
Thiago Almeida <thiago@lizarb.org>
2023-02-26 07:13:40 -0300

    AppGenerator generates an app with only DevSystem

--- lib/dev_system/dev/controllers/generators/app_generator.rb
+++ lib/dev_system/dev/controllers/generators/app_generator.rb
@@ -3,7 +3,10 @@ class DevSystem
 
     def self.call args
       log :higher, "Called #{self}.#{__method__} with args #{args}"
+      new.call args
+    end
 
+    def call args
       # setup
 
       name = args.shift || "app_1"
@@ -20,7 +23,7 @@ class DevSystem
       require "fileutils"
       FileUtils.cp_r from, to, verbose: true
 
-      `cd #{to}; git init .`
+      `cd #{to}; git init -b main`
 
       # extra
 
@@ -29,18 +32,22 @@ class DevSystem
       FileShell.gitkeep "#{to}/lib"
       FileShell.gitkeep "#{to}/tmp"
 
-      FileShell.create_folder "#{to}/app"
+      DirShell.create "#{to}/app"
 
       # systems
 
-      App.systems.keys.each { |system_name| copy to, system_name }
+      # App.systems.keys.each { |system_name| copy to, system_name }
+      copy to, :dev
 
       FileUtils.rm "#{to}/app/dev/commands/new_command.rb"
       FileUtils.rm "#{to}/app/dev/commands/new_command_test.rb"
 
+      `rm -rf #{to}/app/dev/generators/record_generator*`
+      `rm -rf #{to}/app/dev/generators/request_generator*`
+
       FileUtils.cp_r "#{Lizarb::APP_DIR}/.ruby-version", "#{to}/.ruby-version", verbose: true
       FileUtils.cp_r "#{Lizarb::APP_DIR}/README.md", "#{to}/README.md", verbose: true
-      FileUtils.cp_r "#{Lizarb::APP_DIR}/app.rb", "#{to}/app.rb", verbose: true
+      TextShell.write "#{to}/app.rb", render("app.rb")
       FileUtils.cp_r "#{Lizarb::APP_DIR}/app.env", "#{to}/app.env", verbose: true
       FileUtils.cp_r "#{Lizarb::APP_DIR}/app.code.env", "#{to}/app.code.env", verbose: true
 
@@ -49,7 +56,7 @@ class DevSystem
       log "Liza Application initialized at `#{to}`"
     end
 
-    def self.copy name, system_name
+    def copy name, system_name
       from = "#{Lizarb::APP_DIR}/app/#{system_name}_box.rb"
       to = "#{name}/app"
       if File.exist? from
@@ -68,4 +75,22 @@ class DevSystem
     end
 
   end
+end
+
+__END__
+
+# app.rb.erb
+App.call ARGV do
+
+  # Systems help you organize your application's dependencies and RAM memory usage.
+  # Learn more: http://guides.lizarb.org/systems.html
+
+  system :dev
+
+  # Modes help you organize your application's behavior and settings.
+  # Learn more: http://guides.lizarb.org/modes.html
+
+  mode :code
+  mode :demo
+
 end
