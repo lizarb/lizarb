3ba5ffd9c8102427381c41f918457d25f8a50d15
Thiago Pinto <thiagopintodev@gmail.com>
2023-03-05 15:22:21 -0300

    DevSystem LogPanel - prepend process and thread id if not in code mode

--- lib/dev_system/dev/panels/log_panel.rb
+++ lib/dev_system/dev/panels/log_panel.rb
@@ -2,7 +2,12 @@ class DevSystem::LogPanel < Liza::Panel
 
   # https://rubyapi.org/3.1/o/logger
   def call string
-    puts "lizarb #{Lizarb.thread_id.to_s.rjust_zeroes 3} #{string}"
+    unless App.mode == :code
+      pid = Process.pid
+      tid = Lizarb.thread_id.to_s.rjust_zeroes 3
+      string = "#{pid} #{tid} #{string}"
+    end
+    puts string
   end
 
 end
