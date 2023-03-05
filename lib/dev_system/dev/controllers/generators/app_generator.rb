c3e90f2794e47ea08ef666206bd90fd36bac9230
Thiago Pinto <thiagopintodev@gmail.com>
2023-03-05 14:16:07 -0300

    DevSystem AppGenerator - Dropping folder app_new in favor of LERB templates

--- lib/dev_system/dev/controllers/generators/app_generator.rb
+++ lib/dev_system/dev/controllers/generators/app_generator.rb
@@ -19,8 +19,10 @@ class DevSystem::AppGenerator < DevSystem::Generator
 
     # init
 
-    require "fileutils"
-    FileUtils.cp_r from, to, verbose: true
+    DirShell.create to
+    TextShell.write "#{to}/.gitignore", render("hidden.gitignore")
+    TextShell.write "#{to}/Gemfile", render("Gemfile.rb")
+    TextShell.write "#{to}/Procfile", render("Procfile.yml")
 
     `cd #{to}; git init -b main`
 
@@ -91,4 +93,54 @@ App.call ARGV do
   mode :code
   mode :demo
 
+end
+
+# hidden.gitignore.erb
+
+# Ignore all files in all subdirectories
+.gitignore/.bundle/
+/tmp/
+*.sqlite
+*.rdb
+
+# Procfile.yml.erb
+
+<%= "# HEROKU EXAMPLE" %>
+
+web: LIZA_MODE=demo bundle exec liza rack h=0.0.0.0 p=$PORT
+
+# Gemfile.rb.erb
+
+<%= "# frozen_string_literal: true" %>
+
+source "https://rubygems.org"
+
+ruby File.read(".ruby-version").strip
+
+group :default do
+  gem "lizarb", "~> <%= Lizarb::VERSION %>"
+  # gem "lizarb", github: "rubyonrails-brasil/lizarb"
+end
+
+group :dev do
+  # gems you only want to load if DevSystem is loaded
+  # gem "pry", "~> 0.14.1"
+end
+
+group :happy do
+  # gems you only want to load if HappySystem is loaded
+end
+
+group :net do
+  # gems you only want to load if NetSystem is loaded
+  # gem "redis", "~> 5.0"
+  # gem "sqlite3", "~> 1.5"
+end
+
+group :web do
+  # gems you only want to load if WebSystem is loaded
+  # gem "rack", "~> 3.0"
+  # gem "rackup", "~> 0.2.2"
+  # gem "puma", "~> 5.6"
+  # gem "htmlbeautifier", "~> 1.4"
 end
