5f486296f26d82717de57d83863d0ab4353d8c2e
Thiago Almeida <thiago@lizarb.org>
2023-04-08 00:36:50 -0300

    Removing AppBench

--- app/dev/benches/app_bench.rb
+++
@@ -1,5 +0,0 @@
-class AppBench < Liza::Bench
-
-  # Set up your bench controllers per the DSL in http://guides.lizarb.org/controllers/bench.html
-
-end
--- app/dev/benches/app_bench_test.rb
+++
@@ -1,7 +0,0 @@
-class AppBenchTest < Liza::BenchTest
-
-  test :subject_class do
-    assert subject_class == AppBench
-  end
-
-end
