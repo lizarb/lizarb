a20b4cf306925d188248f6d60685e2700d9963bf
Thiago Almeida <thiago@lizarb.org>
2023-04-13 16:39:39 -0300

    ModuleTest - Fixing test

--- lib/liza/ruby_tests/module_test.rb
+++ lib/liza/ruby_tests/module_test.rb
@@ -8,14 +8,14 @@ class Liza::ModuleTest < Liza::RubyTest
     assert Object.source_location == []
 
     assert Liza::Command.source_location[1] == 1
-    assert AppCommand.source_location[1] == 1
+    assert TerminalCommand.source_location[1] == 1
   end
 
   test :source_location_radical do
     assert Object.source_location_radical == nil
 
     assert Liza::Command.source_location_radical.end_with? "/lib/dev_system/dev/controllers/command"
-    assert AppCommand.source_location_radical.end_with? "/app/dev/commands/app_command"
+    assert TerminalCommand.source_location_radical.end_with? "/lib/dev_system/dev/controllers/command/terminal_command"
   end
 
 end
