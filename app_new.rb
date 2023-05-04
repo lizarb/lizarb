fbd0a33d2c0342f020ab2fd137bf78b8f0e2184a
Thiago Pinto <thiagopintodev@gmail.com>
2023-05-04 01:44:21 -0300

    app_new.rb - Removing comments with outdated links

--- app_new.rb
+++ app_new.rb
@@ -4,13 +4,11 @@
 class App
 
   # Modes help you organize your application's behavior and settings.
-  # Learn more: http://guides.lizarb.org/modes.html
 
   mode :code
   mode :demo
 
   # Systems help you organize your application's dependencies and RAM memory usage.
-  # Learn more: http://guides.lizarb.org/systems.html
 
   system :dev
 
