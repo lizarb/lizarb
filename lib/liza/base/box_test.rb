ee954d64261a98338e3329f7bc5149e77c9121c4
Thiago Almeida <thiago@lizarb.org>
2023-07-09 01:33:12 -0300

    Liza Box - assert_sub

--- lib/liza/base/box_test.rb
+++ lib/liza/base/box_test.rb
@@ -3,8 +3,14 @@ class Liza::BoxTest < Liza::UnitTest
     assert subject_class == Liza::Box
   end
 
+  def assert_sub name, controller_class, panel_class, kaller: caller
+    assert_equality subject_class[name].class, panel_class, kaller: kaller
+    assert_equality subject_class.controllers[name], controller_class, kaller: kaller
+  end
+
   test :settings do
     assert subject_class.log_level == :normal
     assert subject_class.log_color == :white
   end
+
 end
