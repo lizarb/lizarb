228be475bb9115791d8ca89a7f90fe7726cf2310
Thiago Pinto <thiagopintodev@gmail.com>
2023-07-09 10:31:40 -0300

    WebSystem WebBox - Organizing Files

--- lib/web_system/dev/web_command.rb
+++
@@ -1,9 +0,0 @@
-class WebSystem::WebCommand < Liza::Command
-
-  def self.call args
-    log "args #{args}"
-    log "DEPRECATED: please use `lizarb rack` instead"
-    RackCommand.call args
-  end
-
-end
