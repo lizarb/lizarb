774e9f2bb0d9c02278f256ff4957f2696da0cd60
Thiago Pinto <thiagopintodev@gmail.com>
2023-07-09 10:32:51 -0300

    WebSystem Sub Request - Organizing Files

---
+++ lib/web_system/sub/request/generators/request_generator_test.rb
@@ -0,0 +1,12 @@
+class WebSystem::RequestGeneratorTest < DevSystem::GeneratorTest
+
+  test :subject_class do
+    assert subject_class == WebSystem::RequestGenerator
+  end
+
+  test :settings do
+    assert subject_class.log_level == :normal
+    assert subject_class.log_color == :blue
+  end
+
+end
