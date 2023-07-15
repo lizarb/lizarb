7c77625e47974c7784a47e8a322cd4b2c1e8f60c
Thiago Pinto <thiagopintodev@gmail.com>
2023-07-15 15:51:35 -0300

    WebSystem RequestCommand - Making usage easier

--- lib/web_system/sub/request/commands/request_command.rb
+++ lib/web_system/sub/request/commands/request_command.rb
@@ -1,76 +1,62 @@
 class WebSystem::RequestCommand < Liza::Command
 
-  VALID_ACTIONS = %w[find get post]
-
-  def call(args)
+  def self.call(args)
     log "args = #{args.inspect}"
+    path = args.first
 
-    @command = args.shift
-    @args = args
-    
-    return help unless VALID_ACTIONS.include? @command
-    perform
-  rescue StandardError => error
-    @error = error
-    handle
-  end
+    help(args) # always show help
 
-  def perform
-    case @command
-    when "find"
-      find
-    when "get"
-      get_request
-    when "post"
-      post_request
+    new.instance_exec do
+      @env = {}
+      @env["REQUEST_PATH"] = path
+
+      request_panel.find @env
+
+      @request_class = @env["LIZA_REQUEST_CLASS"] || raise("No request class found")
+
+      puts render :find, format: :txt
     end
   end
 
-  def handle
-    log render :error, format: :txt
+  def self.help(args)
+    log "args = #{args.inspect}"
+    new.instance_exec do
+      puts render :help, format: :txt
+    end
   end
 
-  def help
-    log render :help, format: :txt
-  end
-
-  # actions
-
-  def find
-    path = @args.first
-
-    @env = {}
-    @env["REQUEST_PATH"] = path
-
-    request_panel.find @env
-
-    @request_class = @env["LIZA_REQUEST_CLASS"] || raise("No request class found")
-
-    puts render :find, format: :txt
-  end
-
-  def get_request
-    path = @args.first
+  def self.get(args)
+    return superclass.method(__method__).call(args) unless args.is_a? Array
+    log "args = #{args.inspect}"
+    path = args.first
+    return help if path.nil?
     
-    @env = {}
-    @env["REQUEST_METHOD"] = "GET"
-    @env["REQUEST_PATH"]   = path
+    new.instance_exec do
+      @env = {}
+      @env["REQUEST_METHOD"] = "GET"
+      @env["REQUEST_PATH"]   = path
 
-    @status, @headers, @body = request_panel.call @env
-    log "STATUS #{@status} with #{@headers.count} headers and a #{@body.first.size} byte body"
-    puts render :response, format: :http
+      @status, @headers, @body = request_panel.call @env
+      log "STATUS #{@status} with #{@headers.count} headers and a #{@body.first.size} byte body"
+      puts render :response, format: :http
+    end
   end
 
-  def post_request
-    path = @args.first
+  def self.post(args)
+    return superclass.method(__method__).call(args) unless args.is_a? Array
+    log "args = #{args.inspect}"
+    path = args.first
+    return help if path.nil?
     
-    @env = {}
-    @env["REQUEST_METHOD"] = "POST"
-    @env["REQUEST_PATH"]   = path
+    new.instance_exec do
+      @env = {}
+      @env["REQUEST_METHOD"] = "POST"
+      @env["REQUEST_PATH"]   = path
 
-    @status, @headers, @body = request_panel.call @env
-    log "STATUS #{@status} with #{@headers.count} headers and a #{@body.first.size} byte body"
-    puts render :response, format: :http
+      @status, @headers, @body = request_panel.call @env
+      log "STATUS #{@status} with #{@headers.count} headers and a #{@body.first.size} byte body"
+      puts render :response, format: :http
+    end
   end
 
   # helpers
@@ -101,9 +87,10 @@ backtrace  <%= @error.backtrace.select { |s| @stop = true if s.include?("/exe/li
 
 USAGE:
 
-liza request find /path/to/action.format
-liza request get /path/to/action.format
-liza request post /path/to/action.format
+liza request /path/to/action.format
+liza request:get /path/to/action.format
+liza request:post /path/to/action.format
+liza request:help
 
 # view find.txt.erb
 
