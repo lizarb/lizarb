6e42af899db3060267dfa0b62dace370fc66a527
Thiago Almeida <thiago@lizarb.org>
2023-08-10 23:04:36 -0300

    Renamed Global Variable - $coding is the new $code_mode

--- lib/dev_system/sub/log/log_panel.rb
+++ lib/dev_system/sub/log/log_panel.rb
@@ -2,7 +2,7 @@ class DevSystem::LogPanel < Liza::Panel
 
   # https://rubyapi.org/3.1/o/logger
   def call string
-    unless $code_mode
+    unless $coding
       pid = Process.pid
       tid = Lizarb.thread_id.to_s.rjust_zeroes 3
       string = "#{pid} #{tid} #{string}"
