58324e62ee4849d62f693b9831e7627ef82427e2
Thiago Almeida <thiago@lizarb.org>
2023-08-16 22:38:27 -0300

    DevSystem TextShell - append_line_after_last_including

--- lib/dev_system/sub/shell/shells/text_shell.rb
+++ lib/dev_system/sub/shell/shells/text_shell.rb
@@ -20,4 +20,29 @@ class DevSystem::TextShell < DevSystem::FileShell
     File.write path, content
   end
 
+  def self.append_line_after_last_including(path:, newline:, after:)
+    log "appending to #{path}"
+    state = nil
+    lines = File.readlines(path)
+    File.open(path, "w") do |out|
+      lines.each do |line|
+        state = :entered_systems_region if state == nil && line =~ after
+        state = :left_systems_region if state == :entered_systems_region && line !~ after
+        
+        if state == :left_systems_region
+          out.write("#{newline}\n")
+          log newline.strip.green if log_append?
+          state = :done
+        end
+        
+        out.write(line)
+        log line.strip.light_black if log_append?
+      end
+    end
+  end
+
+  def self.log_append?
+    true
+  end
+
 end
