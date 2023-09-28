b5111247379b463c001fc2330d8cf41fa28eac0f
Thiago Pinto <thiagopintodev@gmail.com>
2023-09-28 02:21:20 -0300

    DevSystem TextShell - methods accept optional log_level

--- lib/dev_system/sub/shell/shells/text_shell.rb
+++ lib/dev_system/sub/shell/shells/text_shell.rb
@@ -2,20 +2,20 @@ class DevSystem::TextShell < DevSystem::FileShell
     
   set :create_dir, true
   
-  def self.read path
-    log "Reading #{path}"
+  def self.read path, log_level: self.log_level
+    log log_level, "Reading #{path}"
     _raise_if_blank path
     _raise_if_not_exists path
 
     File.read path
   end
   
-  def self.write path, content, create_dir: nil
-    log "Writing #{content.to_s.size} characters (#{content.encoding}) to #{path}"
+  def self.write path, content, create_dir: nil, log_level: self.log_level
+    log log_level, "Writing #{content.to_s.size} characters (#{content.encoding}) to #{path}"
     _raise_if_blank path
 
     create_dir = get :create_dir if create_dir.nil?
-    DevSystem::DirShell.create File.dirname path if create_dir
+    DevSystem::DirShell.create File.dirname(path), log_level: log_level if create_dir
 
     File.write path, content
   end
