c780f46d2fe9fef77681ea9f0a1a73eac8e21975
Thiago Pinto <thiagopintodev@gmail.com>
2023-10-18 19:34:35 -0300

    Liza UnitLoggingPart

--- lib/liza/unit.rb
+++ lib/liza/unit.rb
@@ -34,6 +34,8 @@ class Liza::Unit
 
   part :unit_classes
 
+  part :unit_logging
+
   part :unit_methods
 
   part :unit_procedure
@@ -55,116 +57,6 @@ class Liza::Unit
       else
         Object.const_get "#{name}Test"
       end
-  end
-
-  # LOG
-
-  def self.log_levels()= App::LOG_LEVELS
-  def log_levels()= App::LOG_LEVELS
-
-  def self.log log_level = App::DEFAULT_LOG_LEVEL, object, kaller: caller
-    log_level = log_levels[log_level] if log_level.is_a? Symbol
-    raise "invalid log_level `#{log_level}`" unless log_levels.values.include? log_level
-    return unless log_level? log_level
-
-    env = {}
-    env[:type] = :log
-    env[:unit] = self
-    env[:unit_class] = self
-    env[:message_log_level] = log_level
-    env[:unit_log_level] = self.log_level
-    env[:caller] = kaller
-    env[:object] = object
-
-    DevBox[:log].call env
-  end
-
-  def log log_level = App::DEFAULT_LOG_LEVEL, object, kaller: caller
-    log_level = log_levels[log_level] if log_level.is_a? Symbol
-    raise "invalid log_level `#{log_level}`" unless log_levels.values.include? log_level
-    return unless log_level? log_level
-
-    env = {}
-    env[:type] = :log
-    env[:unit] = self
-    env[:unit_class] = self.class
-    env[:message_log_level] = log_level
-    env[:unit_log_level] = self.log_level
-    env[:caller] = kaller
-    env[:object] = object
-
-    DevBox[:log].call env
-  end
-
-  def self.stick *args
-    StickLog.new *args
-  end
-
-  def stick *args
-    StickLog.new *args
-  end
-
-  #
-
-  def self.log_level new_value = nil
-    if new_value
-      new_value = log_levels[new_value] if new_value.is_a? Symbol
-      raise "invalid log_level `#{new_value}`" unless log_levels.values.include? new_value
-      set :log_level, new_value
-    else
-      get :log_level
-    end
-  end
-
-  def log_level new_value = nil
-    if new_value
-      new_value = log_levels[new_value] if new_value.is_a? Symbol
-      raise "invalid log_level `#{new_value}`" unless log_levels.values.include? new_value
-      set :log_level, new_value
-    else
-      get :log_level
-    end
-  end
-
-  def self.log_hash hash, prefix: "", kaller: caller[1..-1]
-    prefix = prefix.to_s
-    size = hash.keys.map(&:to_s).map(&:size).max
-    
-    hash.each do |k,v|
-      log "#{prefix}#{k.to_s.ljust size} = #{v.to_s.inspect}", kaller: kaller
-    end
-  end
-
-  def log_hash hash, prefix: "", kaller: caller[1..-1]
-    self.class.log_hash hash, prefix: prefix, kaller: kaller
-  end
-
-  def self.log_array array, prefix: "", kaller: caller[1..-1]
-    prefix = prefix.to_s
-    size = array.size.to_s.size+1
-    
-    array.each.with_index do |v, i|
-      log "#{prefix}#{i.to_s.ljust size} = #{v.inspect}", kaller: kaller
-    end
-  end
-
-  def log_array array, prefix: "", kaller: caller[1..-1]
-    self.class.log_array array, prefix: prefix, kaller: kaller
-  end
-
-  #
-
-  def self.log?(log_level = App::DEFAULT_LOG_LEVEL)= log_level? log_level
-  def log?(...)= log_level? log_level
-
-  def self.log_level? log_level = App::DEFAULT_LOG_LEVEL
-    log_level = log_levels[log_level] if log_level.is_a? Symbol
-    log_level >= self.log_level
-  end
-
-  def log_level? log_level = App::DEFAULT_LOG_LEVEL
-    log_level = log_levels[log_level] if log_level.is_a? Symbol
-    log_level >= self.log_level
   end
 
   # SYSTEM
---
+++ lib/liza/unit_parts/unit_logging_part_test.rb
@@ -0,0 +1,3 @@
+class Liza::UnitLoggingPartTest < Liza::UnitTest
+
+end
