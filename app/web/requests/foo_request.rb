dbbe8f09b21aded5701cac7771876f62693800a2
Thiago Almeida <thiago@lizarb.org>
2023-10-31 03:30:30 -0300

    WebSystem SimpleRequest

---
+++ app/web/requests/foo_request.rb
@@ -0,0 +1,26 @@
+class FooRequest < Liza::SimpleRequest
+
+  # GET /foo
+
+  def call_index
+    log "."
+  end
+
+  # POST /foo
+
+  def call_index!
+    log "."
+  end
+
+end
+
+__END__
+
+# view index.html.erb
+<h1><%= self.class %></h1>
+<h2><%= action %></h2>
+<p>request: <%= request %></p>
+<p>action: <%= action %></p>
+<p>format: <%= format %></p>
+<p>qs: <%= qs %></p>
+<p>segments: <%= segments %></p>
