bc7e4280bb479365b0598d51e8343e7f89d22940
Thiago Pinto <thiagopintodev@gmail.com>
2023-10-31 03:35:37 -0300

    LabSystem KrokiDockerShell KrokiClient LizaKrokiClient KrokiRequest

---
+++ lib/lab_system/clients/kroki_client_test.rb
@@ -0,0 +1,32 @@
+class LabSystem::KrokiClientTest < NetSystem::ClientTest
+
+  test :subject_class do
+    assert_equality subject_class, LabSystem::KrokiClient
+  end
+
+  test :settings do
+    assert_equality subject_class.log_level, 0
+  end
+
+  #
+
+  test :call do
+    result = subject_class.call(:nomnoml, :ab, :svg)
+    assert result.start_with? "<svg"
+  end
+
+  #
+
+  test :nomnoml do
+    result = subject_class.nomnoml(:ab, :svg)
+    assert result.start_with? "<svg"
+  end
+
+  test :plantuml do
+    result = subject_class.plantuml(:ab, :svg)
+    unless assert result.start_with? %|<?xml version="1.0" encoding="us-ascii" standalone="no"?><svg|
+      puts result
+    end
+  end
+
+end
