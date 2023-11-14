65bbaac2202af8529b03e676da3806b7806c5f68
Thiago Almeida <thiago@lizarb.org>
2023-11-14 03:58:15 -0300

    examples - NarrativeMethodCommand inherits from DevSystem Command

--- examples/command/app/dev/commands/narrative_method_command_test.rb
+++ examples/command/app/dev/commands/narrative_method_command_test.rb
@@ -1,12 +1,11 @@
-class NarrativeMethodCommandTest < Liza::CommandTest
+class NarrativeMethodCommandTest < DevSystem::CommandTest
 
   test :subject_class do
     assert subject_class == NarrativeMethodCommand
   end
 
   test :settings do
-    assert subject_class.log_level == :normal
-    assert subject_class.log_color == :green
+    assert subject_class.log_level == 0
   end
 
   test :validate, :not_implemented do
