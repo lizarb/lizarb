05bbb349ac3e37f0abcfb4c3438427cdb9d2a3f7
Thiago Almeida <thiago@lizarb.org>
2023-11-14 17:24:09 -0300

    DevSystem GeneratorGenerator - call_default

--- lib/dev_system/sub/generator/generators/generator_generator.rb
+++ lib/dev_system/sub/generator/generators/generator_generator.rb
@@ -1,181 +1,71 @@
-class DevSystem::GeneratorGenerator < Liza::Generator
+class DevSystem::GeneratorGenerator < DevSystem::SimpleGenerator
 
-  # lizarb generator NAME
+  # liza g generator name place=app 
 
-  def self.call args
-    log "args = #{args.inspect}"
-    log "This generator in under construction. Use 'lizarb generator:examples' instead."
-  end
+  def call_default
+    @controller_class = Generator
+    
+    name!
+    place!
 
-  # lizarb generator:install
-
-  def self.install(args)
-    log "args = #{args.inspect}"
-    log "This generator in under construction. Use 'lizarb generator:examples' instead."
-  end
-
-  # lizarb generator:examples
-
-  def self.examples(args)
-    log "args = #{args.inspect}"
-
-    generators = _build_example_generators args
-    generators.each do |generator|
-      generator.generate!
+    ancestor = SimpleGenerator
+    create_controller @name, @controller_class, @place, @path, ancestor: ancestor do |unit, test|
+      unit.section :controller_section_1, caption: "liza g #{@name} name place=app"
+      unit.section :controller_section_2, caption: "liza g #{@name}:examples"
+      unit.view    :generator_view_1, key: :controller_section_1
+      test.section :controller_test_section_1
     end
-  end
-
-  def self._build_example_generators(args)
-    ret = []
-
-    app_name = $APP
-
-    root_path = Lizarb::IS_LIZ_DIR ? Lizarb::CUR_DIR : Lizarb::GEM_DIR
-
-    #
-
-    ret << new.tap do |instance|
-      instance.instance_exec do
-        @content = TextShell.read "#{root_path}/lib/dev_system/sub/bench/generators/bench_generator.rb"
-
-        @content = @content.split("\n").each do |line|
-          line.gsub! "class DevSystem::BenchGenerator < DevSystem::Generator", "class BenchGenerator < Liza::BenchGenerator"
-        end.join("\n")
-
-        @path = "#{app_name}/dev/generators/bench_generator.rb"
-        @args = args
-      end
-    end
-
-    ret << new.tap do |instance|
-      instance.instance_exec do
-        @content = TextShell.read "#{root_path}/lib/dev_system/sub/bench/generators/bench_generator_test.rb"
-
-        @content = @content.split("\n").each do |line|
-          line.gsub! "class DevSystem::BenchGeneratorTest < DevSystem::GeneratorTest", "class BenchGeneratorTest < Liza::BenchGeneratorTest"
-          line.gsub! "assert subject_class == DevSystem::BenchGenerator", "assert subject_class == ::BenchGenerator"
-        end.join("\n")
-
-        @path = "#{app_name}/dev/generators/bench_generator_test.rb"
-        @args = args
-      end
-    end
-
-    #
-
-    ret << new.tap do |instance|
-      instance.instance_exec do
-        @content = TextShell.read "#{root_path}/lib/dev_system/sub/command/generators/command_generator.rb"
-
-        @content = @content.split("\n").each do |line|
-          line.gsub! "class DevSystem::CommandGenerator < DevSystem::ControllerGenerator", "class CommandGenerator < Liza::CommandGenerator"
-        end.join("\n")
-
-        @path = "#{app_name}/dev/generators/command_generator.rb"
-        @args = args
-      end
-    end
-
-    ret << new.tap do |instance|
-      instance.instance_exec do
-        @content = TextShell.read "#{root_path}/lib/dev_system/sub/command/generators/command_generator_test.rb"
-
-        @content = @content.split("\n").each do |line|
-          line.gsub! "class DevSystem::CommandGeneratorTest < DevSystem::ControllerGeneratorTest", "class CommandGeneratorTest < Liza::CommandGeneratorTest"
-          line.gsub! "assert subject_class == DevSystem::CommandGenerator", "assert subject_class == ::CommandGenerator"
-        end.join("\n")
-
-        @path = "#{app_name}/dev/generators/command_generator_test.rb"
-        @args = args
-      end
-    end
-
-    #
-
-    ret << new.tap do |instance|
-      instance.instance_exec do
-        @content = TextShell.read "#{root_path}/lib/dev_system/sub/generator/generators/system_generator.rb"
-
-        @content = @content.split("\n").each do |line|
-          line.gsub! "class DevSystem::SystemGenerator < DevSystem::Generator", "class SystemGenerator < Liza::SystemGenerator"
-        end.join("\n")
-
-        @path = "#{app_name}/dev/generators/system_generator.rb"
-        @args = args
-      end
-    end
-
-    ret << new.tap do |instance|
-      instance.instance_exec do
-        @content = TextShell.read "#{root_path}/lib/dev_system/sub/generator/generators/system_generator_test.rb"
-
-        @content = @content.split("\n").each do |line|
-          line.gsub! "class DevSystem::SystemGeneratorTest < DevSystem::GeneratorTest", "class SystemGeneratorTest < Liza::SystemGeneratorTest"
-          line.gsub! "assert subject_class == DevSystem::SystemGenerator", "assert subject_class == ::SystemGenerator"
-        end.join("\n")
-
-        @path = "#{app_name}/dev/generators/system_generator_test.rb"
-        @args = args
-      end
-    end
-
-    #
-
-    ret
-  end
-
-  #
-
-  def generate!
-    @content ||= render :unit, @template, format: :rb
-    TextShell.write @path, @content if _should_generate?
-  end
-
-  def _should_generate?
-    found = File.exist? @path
-    return true if not found
-
-    _output_diff
-    log "#{@path} already exists. #{"Do you want to overwrite?".underline}"
-
-    title = "Do you want to overwrite #{@path} ?"
-    options = ["Yes", "No"]
-
-    x = DevBox.pick_one title, options
-    x == "Yes"
-  end
-
-  def _output_diff
-    log "current content".red.underline
-    puts File.read(@path).red
-
-    log "new content".green.underline
-    puts @content.green
   end
 
 end
 
 __END__
 
-# view unit.rb.erb
-class <%= @class_name %> < <%= @superclass_name %>
-  <%= render -%>
-end
-# view system.rb.erb
-class Error < Liza::Error; end
+# view controller_section_1.rb.erb
 
+  def call_default
+    @controller_class = <%= @name.camelize %>
+
+    name!
+    place!
+
+    create_controller @name, @controller_class, @place, @path do |unit, test|
+      unit.section :controller_section_1
+      test.section :controller_test_section_1
+    end
+  end
+# view controller_section_2.rb.erb
+
+  # def call_examples
+  #   copy_examples <%= @name.camelize %>
+  # end
+# view generator_view_1.rb.erb
+  def self.call(env)
+    # 
+  end
+
+<%= "#" %> view controller_test_section_1.rb.erb
+
+  test :subject_class, :subject do
+    assert_equality <%%= @class_name %>, subject_class
+    assert_equality <%%= @class_name %>, subject.class
+  end
+
+  # test :subject_class, :call do
+  #   a = 1
+  #   b = 2
+  #   assert_equality a, b
+  # end
   #
+  # test :subject, :call do
+  #   a = 1
+  #   b = 2
+  #   assert_equality a, b
+  # end
 
-  color :white
-
-# view box.rb.erb
-
-  #
-
-# view box_test.rb.erb
+# view controller_test_section_1.rb.erb
 
   test :subject_class, :subject do
     assert_equality <%= @class_name %>, subject_class
     assert_equality <%= @class_name %>, subject.class
   end
-
