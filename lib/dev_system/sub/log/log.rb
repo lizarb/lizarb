2ced86c0127dcf270eae1ab24ba1c8da2ad4c394
Thiago Pinto <thiagopintodev@gmail.com>
2023-11-26 03:38:31 -0300

    DevSystem Log - removing old precaution

--- lib/dev_system/sub/log/log.rb
+++ lib/dev_system/sub/log/log.rb
@@ -1,11 +1,3 @@
 class DevSystem::Log < Liza::Controller
 
-  def self.log(*args)
-    raise NotImplementedError, "method log cannot exist on #{self}"
-  end
-  
-  def log(*args)
-    raise NotImplementedError, "method log cannot exist on #{self}"
-  end
-  
 end
