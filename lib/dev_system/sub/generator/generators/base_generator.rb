959ef43f0ee2dbd082ff1a0347e043b4c2dcbb68
Thiago Almeida <thiago@lizarb.org>
2023-12-04 03:42:55 -0300

    DevSystem - Generator BaseGenerator get_generator_signatures

--- lib/dev_system/sub/generator/generators/base_generator.rb
+++ lib/dev_system/sub/generator/generators/base_generator.rb
@@ -43,4 +43,22 @@ class DevSystem::BaseGenerator < DevSystem::Generator
     env[:command]
   end
 
+  #
+
+  def self.get_generator_signatures
+    signatures = []
+    ancestors_until(BaseGenerator).each do |c|
+      signatures +=
+        c.instance_methods_defined.select do |name|
+          name.start_with? "call_"
+        end.map do |name|
+          OpenStruct.new({
+            name: ( name.to_s.sub("call_", "").sub("default", "") ),
+            description: "# no description",
+          })
+        end
+    end
+    signatures
+  end
+
 end
