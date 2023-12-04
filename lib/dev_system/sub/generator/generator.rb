5c5b10c98b2aecadb7331cb8b8d0561adaf2a108
Thiago Pinto <thiagopintodev@gmail.com>
2023-12-04 03:42:55 -0300

    DevSystem - Generator BaseGenerator get_generator_signatures

--- lib/dev_system/sub/generator/generator.rb
+++ lib/dev_system/sub/generator/generator.rb
@@ -1,3 +1,19 @@
 class DevSystem::Generator < Liza::Controller
 
+  def self.get_generator_signatures
+    signatures = []
+    ancestors_until(Generator).each do |c|
+      signatures +=
+        c.methods_defined.select do |name|
+          c.method(name).parameters == [[:req, :args]]
+        end.map do |name|
+          OpenStruct.new({
+            name: ( name == :call ? "" : name.to_s ),
+            description: "# no description",
+          })
+        end
+    end
+    signatures
+  end
+
 end
