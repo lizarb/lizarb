3e495274fad788433c1ce61c7ae7c4bfca1131bf
Thiago Pinto <thiagopintodev@gmail.com>
2023-12-28 01:25:03 -0300

    DevSystem ShellCommand - output more information

--- lib/dev_system/sub/shell/commands/shell_command.rb
+++ lib/dev_system/sub/shell/commands/shell_command.rb
@@ -3,22 +3,26 @@ class DevSystem::ShellCommand < DevSystem::Command
   def call args
     puts
     puts "Liza         #{ Liza.source_location[0] }"
-    puts "App.folder   #{ App.folder }"
+    puts
+    puts "App.type     #{ App.type.inspect }"
+    puts
+    puts "App.folder   #{ App.folder || "nil" }"
     puts "App.root     #{ App.root }/"
-    puts "App.path     #{ App.path }/"
+    puts "App.path     #{ App.path ? "#{App.path}/" : "nil" }"
     puts
 
     largest_system_name = AppShell.consts[:systems].keys.map(&:to_s).map(&:size).max
-
     AppShell.consts[:systems].each do |system_name, tree_system|
       system = tree_system["system"][0]
-      length = largest_system_name + 6
+      length = largest_system_name + 14
 
-      puts "#{ 
-        "#{system.to_s}"
+      puts "#{
+        "#{system.token == :dev ? "App.system" : "          "}   #{system.token.inspect}"
           .ljust(length)
-          .sub(system.to_s, color(system).to_s)
-       } #{ system.source_location[0] }"
+          .sub(system.token.inspect, (stick system.token.inspect, system.color).to_s)
+      } #{
+        system.source_location[0]
+      }"
     end
 
     puts
