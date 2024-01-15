3842b83a9e12188fdefd296d431fe862fb6d06d2
Thiago Pinto <thiagopintodev@gmail.com>
2024-01-15 01:50:36 -0300

    Module - source_location_path

--- lib/lizarb/ruby/module.rb
+++ lib/lizarb/ruby/module.rb
@@ -2,14 +2,21 @@
 
 class Module
 
-  # /path/to/liza.rb
+  # ["/path/to/liza.rb", 1]
   def source_location
     Array Object.const_source_location name
   end
 
-  # /path/to/liza
+  # "/path/to/liza.rb"
+  def source_location_path
+    source_location[0]
+  rescue
+    nil
+  end
+
+  # "/path/to/liza"
   def source_location_radical
-    source_location[0][0..-4]
+    source_location_path[0..-4]
   rescue
     nil
   end
