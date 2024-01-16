5fafe26e4fd8c9504ee91213a1a33949d90335d1
Thiago Almeida <thiago@lizarb.org>
2024-01-16 14:03:20 -0300

    DevSystem Shell - fixing tests

--- lib/dev_system/sub/shell/shell_test.rb
+++ lib/dev_system/sub/shell/shell_test.rb
@@ -5,7 +5,7 @@ class DevSystem::ShellTest < Liza::ControllerTest
   end
 
   test_methods_defined do
-    on_self :cruby?, :engine, :jruby?, :linux?, :mac?, :os, :ruby_version, :unix?, :windows?
+    on_self :all, :cruby?, :engine, :jruby?, :linux?, :mac?, :os, :ruby_version, :unix?, :windows?
     on_instance
   end
 
