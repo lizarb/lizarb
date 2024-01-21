76406a5bdcb19fbba368c5fa8a09b203f559cf2e
Thiago Pinto <thiagopintodev@gmail.com>
2024-01-20 22:46:08 -0300

    lizarb/script - small bug fix

--- lib/lizarb/script.rb
+++ lib/lizarb/script.rb
@@ -15,9 +15,10 @@ module Lizarb
     raise Lizarb::Error, "Lizarb.script does not support app_global, use Lizarb.sfa" if app == "app_global"
     raise Lizarb::Error, "#{app.inspect} does not start with 'app_'" unless app == "app" or app.to_s.start_with? "app_"
 
-    root, script = cl.absolute_path.split("/", 2)
+    segments = cl.absolute_path.split("/")
+    root, script = segments[0..-3].join("/"), segments[-2..-1].join("/")
 
-    Lizarb.setup_script pwd, script: script, script_app: app
+    Lizarb.setup_script root, script: script, script_app: app
 
     App.class_exec do
       self.systems.clear if systems.any?
