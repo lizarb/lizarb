b900eba4a4350530d506b0f1372f308b5f911154
Thiago Almeida <thiago@lizarb.org>
2024-02-23 01:38:44 -0300

    NetSystem DatabaseCommand - use Database controllers current method

--- lib/net_system/sub/database/commands/database_command.rb
+++ lib/net_system/sub/database/commands/database_command.rb
@@ -4,16 +4,11 @@ class NetSystem::DatabaseCommand < DevSystem::Command
     log "args = #{args.inspect}"
 
     ruby_time   = Time.now
-    redis_time  = NetBox[:database].redis.new.now
-    mongo_time  = NetBox[:database].mongo.new.now
-    sqlite_time = NetBox[:database].sqlite.new.now
-    mysql_time  = NetBox[:database].mysql.new.now
-    pgsql_time  = NetBox[:database].pgsql.new.now
-    # redis_time  = ::RedisDb.current.now
-    # mongo_time  = ::MongoDb.current.now
-    # sqlite_time = ::SqliteDb.current.now
-    # mysql_time  = ::MysqlDb.current.now
-    # pgsql_time  = ::PgsqlDb.current.now
+    redis_time  = RedisDb.current.now
+    mongo_time  = MongoDb.current.now
+    sqlite_time = SqliteDb.current.now
+    mysql_time  = MysqlDb.current.now
+    pgsql_time  = PgsqlDb.current.now
 
     log "Time for Ruby:    #{_red ruby_time}"
     log "Time for Redis:   #{_red redis_time}"
