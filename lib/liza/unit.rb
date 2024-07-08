b478d38f48d1404da2c758956ab90260150d2341
Thiago Almeida <thiago@lizarb.org>
2024-07-08 16:00:15 -0600

    Liza UnitClassesPart - removed and moved methods to Liza Unit

--- lib/liza/unit.rb
+++ lib/liza/unit.rb
@@ -30,8 +30,6 @@ class Liza::Unit
 
   # PARTS
 
-  part :unit_classes
-
   part :unit_logging
 
   part :unit_methods
@@ -43,6 +41,36 @@ class Liza::Unit
   part :unit_renderer
 
   part :unit_setting
+
+  def self.subclasses_select system:
+    subclasses.select { _1.system? system }
+  end
+
+  def self.descendants_select system:
+    descendants.select { _1.system? system }
+  end
+
+  def self.namespace
+    return @namespace if defined? @namespace
+
+    @namespace ||= Object unless to_s.include? "::"
+    @namespace ||= Liza if to_s.start_with? "Liza::"
+    @namespace ||= system
+    @namespace
+  end
+
+  def self.system? system
+    system = Liza.const "#{system}_system" if system.is_a? Symbol
+    system == get(:system)
+  end
+
+  def self.reload!
+    Lizarb.reload
+  end
+
+  def reload!
+    Lizarb.reload
+  end
 
   # TEST
 
