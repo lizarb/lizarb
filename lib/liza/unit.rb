96ac547832e08349e4bb7a6a3c84ffc06a7df4e2
Thiago Almeida <thiago@lizarb.org>
2024-07-08 16:06:36 -0600

    Liza UnitAssociatingPart

--- lib/liza/unit.rb
+++ lib/liza/unit.rb
@@ -30,6 +30,8 @@ class Liza::Unit
 
   # PARTS
 
+  part :unit_associating
+
   part :unit_logging
 
   part :unit_methods
@@ -42,28 +44,6 @@ class Liza::Unit
 
   part :unit_setting
 
-  def self.subclasses_select system:
-    subclasses.select { _1.system? system }
-  end
-
-  def self.descendants_select system:
-    descendants.select { _1.system? system }
-  end
-
-  def self.namespace
-    return @namespace if defined? @namespace
-
-    @namespace ||= Object unless to_s.include? "::"
-    @namespace ||= Liza if to_s.start_with? "Liza::"
-    @namespace ||= system
-    @namespace
-  end
-
-  def self.system? system
-    system = Liza.const "#{system}_system" if system.is_a? Symbol
-    system == get(:system)
-  end
-
   def self.reload!
     Lizarb.reload
   end
@@ -71,42 +51,6 @@ class Liza::Unit
   def reload!
     Lizarb.reload
   end
-
-  # TEST
-
-  def self.test_class
-    @test_class ||=
-      if first_namespace == "Liza"
-        Liza.const_get "#{last_namespace}Test"
-      else
-        Object.const_get "#{name}Test"
-      end
-  end
-
-  # CONTROLLER
-
-  def self.division
-    Liza::Controller
-  end
-
-  # SYSTEM
-
-  def self.system
-    if name&.include? "::"
-      return System if first_namespace == "Liza"
-      Object.const_get first_namespace
-    else
-      superclass.system
-    end
-  end
-
-  singleton_class.send :public, :system
-
-  def system
-    self.class.system
-  end
-
-  public :system
 
   #
 
