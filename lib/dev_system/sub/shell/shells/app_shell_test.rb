773ba3b0bb671ff0cf88a0af6890a17bc81a252b
Thiago Almeida <thiago@lizarb.org>
2024-07-27 16:33:31 -0600

    DevSystem AppShell - adjuting liza_categories

--- lib/dev_system/sub/shell/shells/app_shell_test.rb
+++ lib/dev_system/sub/shell/shells/app_shell_test.rb
@@ -14,7 +14,7 @@ class DevSystem::AppShellTest < DevSystem::ShellTest
     system_keys = ["dev", "happy", "net", "web", "work", "micro", "desk", "crypto", "media", "art", "deep", "lab"]
     assert_equality subject.consts.keys,           [:top_level, :liza, :systems, :app]
     assert_equality subject.consts[:top_level],    [Lizarb, App, Liza]
-    assert_equality subject.consts[:liza].keys,    ["unit", "base", "safety", "meta", "ruby_tests"]
+    assert_equality subject.consts[:liza].keys,    ["unit", "helper_units", "systemic_units", "subsystemic_units", "ruby_tests"]
     assert_equality subject.consts[:systems].keys, system_keys
     assert_equality subject.consts[:app].keys,     system_keys
   end
