2c0d527b5b741ff6146c3fd769f9aec5d78a72c4
Thiago Almeida <thiago@lizarb.org>
2024-08-21 15:16:46 -0600

    DevSystem SimpleCommand - simple_arg simple_string simple_boolean require requirements

--- lib/dev_system/sub/command/commands/simple_command.rb
+++ lib/dev_system/sub/command/commands/simple_command.rb
@@ -1,38 +1,66 @@
 class DevSystem::SimpleCommand < DevSystem::BaseCommand
 
-  #
+  # This gem will be lazily required
+  def self.require(gem_name)= add :requirements, gem_name
 
-  def call(env)
-    env[:simple] = [""]
-    super
+  def self.requirements()= get :requirements
+  
+  def self.require_requirements()= requirements&.each { Kernel.require _1 }&.clear
+
+  def self.before
+    require_requirements
   end
 
-  #
+  def before
+    simple_strings  = args.select { |arg| arg.include? "=" }
+    simple_booleans = args.select { |arg| ["+", "-"].any? { arg.start_with? _1 }  }
 
+    env[:simple] = [""]
+    env[:simple_args] = args - simple_strings - simple_booleans
+    env[:simple_strings] = simple_strings.map { |arg| arg.split "=" }.map { |k,v| [k.to_sym, v] }.to_h
+    env[:simple_booleans] = simple_booleans.map { |arg| [arg[1..-1], arg[0]=="+"] }.map { |k,v| [k.to_sym, v] }.to_h
+  end
+
+  # Logs the current state of the :remember key in the environment.
   def log_simple_remember
     log :higher, "env[:remember] is now #{stick system.color, (env[:simple].join " ")}", kaller: caller
   end
   
+  # Retrieves the value of an arg key=value.
+  # If a block is given and the key is not present,
+  # the block's result is used as the value.
   # 
+  # @param key [Symbol] The key to retrieve from the environment.
+  # @yield [String] A block to generate a value if the key is not present.
+  # @return [String] The value associated with the key or the result of the block if the key is absent.
+  def simple_string(key)
+    # Find the value associated with the key in simple_strings.
+    string = simple_strings[key.to_sym]
+    log "#{key}=#{string.inspect}"
 
-  # key=value
-  def simple_string(key, &block)
-    string = env[:args].find { _1.start_with? "#{key}=" }.to_s.sub("#{key}=", "")
-    return string unless string.empty?
+    # If no block is given or a value is already found, return the value.
+    return string unless block_given?
 
-    value = yield.to_s.split(" ")[0]
+    # If the value is not present and a block is given, compute the new value.
+    return string if string
+
+    value = yield.to_s.split(" ").first
     log :low, value.inspect
 
+    # Remember this value
     env[:simple] << "#{key}=#{value}"
-
     log_simple_remember
 
     value
   end
 
-  #
-
-  # key=value
+  # Retrieves the value of an arg key=value.
+  # If a block is given and the key is not present,
+  # a color picker will be called to select a color.
+  # 
+  # @param key [Symbol] The key to retrieve from the environment.
+  # @param string [String] A prompt string for color selection.
+  # @return [Symbol] The selected color as a symbol.
   def simple_color(key, string: "I LOVE RUBY")
     simple_string key do
       TtyInputCommand.pick_color string: string
@@ -40,8 +68,6 @@ class DevSystem::SimpleCommand < DevSystem::BaseCommand
       log :low, color.inspect
     end
   end
-
-  #
 
   # key=value
   def simple_controller_placement(key, places)
@@ -58,20 +84,34 @@ class DevSystem::SimpleCommand < DevSystem::BaseCommand
     end
   end
 
-  #
+  # Returns all simple arguments (i.e., args that are not key=value or +key/-key).
+  # 
+  # @return [Array<String>] An array of simple arguments.
+  def simple_args()= env[:simple_args]
 
-  # arg0 arg1 arg2
-  def simple_args
-    env[:args]
-      .reject { _1.start_with? "+" }
-      .reject { _1.start_with? "-" }
-      .reject { _1.include? "=" }
-  end
+  # Retrieves all simple boolean arguments from the environment.
+  # 
+  # @return [Hash<Symbol, Boolean>] A hash where each key is a symbol representing a boolean argument.
+  def simple_booleans()= env[:simple_booleans]
 
-  # arg0 arg1 arg2
+  # Retrieves all simple string arguments from the environment.
+  # 
+  # @return [Hash<Symbol, String>] A hash where each key is a symbol representing a string argument.
+  def simple_strings()= env[:simple_strings]
+
+  # Retrieves the value of a specific simple argument by index.
+  # If the argument is not present, a block will be executed to generate a value.
+  # 
+  # @param index [Integer] The index of the argument to retrieve.
+  # @yield [String] A block to generate a value if the argument is not present.
+  # @return [String] The argument value or the result of the block if the argument is absent.
   def simple_arg(index, &block)
-    string = env[:args][index]
-    return string if string
+    # Find the value associated with the index in simple_args.
+    string = simple_args[index]
+    log simple_args.map.with_index { |arg, i| i == index ? "[#{arg}]" : arg }.join " "
+
+    # If no block is given or a value is already found, return the value.
+    return string unless block_given?
 
     value = yield
     value = "" if value.nil?
@@ -87,18 +127,23 @@ class DevSystem::SimpleCommand < DevSystem::BaseCommand
     value
   end
 
-  #
-
-  # arg0 arg1 arg2
+  # Prompts the user to input a value for a specific argument by index.
+  # 
+  # @param index [Integer] The index of the argument to retrieve.
+  # @param title [String] The prompt title for user input.
+  # @return [String] The user-provided value.
   def simple_arg_ask(index, title)
     simple_arg index do
       TtyInputCommand.prompt.ask(title)
     end
   end
 
-  #
-
-  # arg0 arg1 arg2
+  # May prompt the user to input a value for a specific argument by index, enforcing snake_case validation.
+  # 
+  # @param index [Integer] The index of the argument to retrieve.
+  # @param title [String] The prompt title for user input.
+  # @param regexp [Regexp] The regular expression for validating input (defaults to /^[a-zA-Z_]*$/).
+  # @return [String] The user-provided value in snake_case format.
   def simple_arg_ask_snakecase(index, title, regexp: /^[a-zA-Z_]*$/)
     simple_arg index do
       TtyInputCommand.prompt.ask(title) do |q|
@@ -108,33 +153,42 @@ class DevSystem::SimpleCommand < DevSystem::BaseCommand
     end
   end
 
-  #
-
-  # +key -key
+  # May prompt the user (default true) unless arg +key or -key is found.
+  # 
+  # @param key [Symbol] The key to retrieve the argument.
+  # @param title [String] The prompt title for user input.
+  # @return [Boolean] The boolean value determined by user input.
   def simple_boolean_yes(key, title)
     simple_boolean key do
       TtyInputCommand.prompt.yes? title
     end
   end
 
-  # +key -key
+  # May prompt the user (default false) unless arg +key or -key is found.
+  # 
+  # @param key [Symbol] The key to retrieve the argument.
+  # @param title [String] The prompt title for user input.
+  # @return [Boolean] The boolean value determined by user input.
   def simple_boolean_no(key, title)
     simple_boolean key do
       TtyInputCommand.prompt.no? title
     end
   end
 
-  # +key -key
+  # Retrieves true or false if arg +key or -key
+  # If a block is given and the key is not present,
+  # the block's result is used as the value.
+  # 
+  # @param key [Symbol] The key to retrieve the argument.
   def simple_boolean(key, &block)
-    return true  if env[:args].find { _1 == "+#{key}" }
-    return false if env[:args].find { _1 == "-#{key}" }
+    return simple_booleans[key] if simple_booleans.key? key
+    return false unless block_given?
 
     value = yield
     log :low, value.inspect
 
     env[:simple] << "+#{key}" if TrueClass === value
     env[:simple] << "-#{key}" if FalseClass === value
-
     log_simple_remember
 
     value
--- lib/dev_system/sub/command/commands/simple_command_test.rb
+++ lib/dev_system/sub/command/commands/simple_command_test.rb
@@ -4,4 +4,55 @@ class DevSystem::SimpleCommandTest < DevSystem::BaseCommandTest
     assert_equality subject_class, DevSystem::SimpleCommand
   end
 
+  def subject_with *args
+    @subject = subject_class.new
+    @subject.instance_exec { @env = DevBox[:command].build_env ["simple", *args] }
+    @subject.before
+  end
+
+  before do
+    subject_with "k1=v1", "k3=v3", "la", "le", "li", "+a", "+b", "-c", "-d"
+  end
+
+  test :simple_strings do
+    assert_equality subject.simple_strings, {:k1=>"v1", :k3=>"v3"}
+  end
+
+  test :simple_string do
+    # Test when the key-value pair is present in the args
+    assert_equality subject.simple_string(:k1), "v1"
+    assert_equality subject.simple_string(:k3), "v3"
+
+    # Test when the key-value pair is not present in the args and a block is given
+    assert_equality subject.simple_string(:k2) { "default" }, "default"
+
+    # Test when the key-value pair is not present in the args and a block is given that returns nil
+    assert_equality subject.simple_string(:k2) { nil }, nil
+
+    # Test when the key-value pair is not present in the args and a block is not given
+    assert_equality subject.simple_string(:k2), nil
+  end
+
+  test :simple_booleans do
+    assert_equality subject.simple_booleans, {:a=>true, :b=>true, :c=>false, :d=>false}
+  end
+
+  test :simple_boolean do
+    assert_equality subject.simple_boolean(:a), true
+    assert_equality subject.simple_boolean(:b), true
+    assert_equality subject.simple_boolean(:c), false
+    assert_equality subject.simple_boolean(:d), false
+  end
+
+  test :simple_args do
+    assert_equality subject.simple_args, ["la", "le", "li"]
+  end
+
+  test :simple_arg do
+    assert_equality subject.simple_arg(0), "la"
+    assert_equality subject.simple_arg(1), "le"
+    assert_equality subject.simple_arg(2), "li"
+    assert_equality subject.simple_arg(3), nil
+  end
+
 end
