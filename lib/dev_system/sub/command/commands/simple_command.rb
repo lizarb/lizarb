883c9f52ae66cb0d79b823caba1306d782312b12
Thiago Almeida <thiago@lizarb.org>
2024-09-05 02:07:07 -0600

    DevSystem SimpleCommand - simple_args small bug fix

--- lib/dev_system/sub/command/commands/simple_command.rb
+++ lib/dev_system/sub/command/commands/simple_command.rb
@@ -5,7 +5,7 @@ class DevSystem::SimpleCommand < DevSystem::BaseCommand
     simple_booleans = args.select { |arg| ["+", "-"].any? { arg.start_with? _1 }  }
 
     env[:simple] = [""]
-    env[:simple_args] = (Array args[1..-1]) - simple_strings - simple_booleans
+    env[:simple_args] = args - simple_strings - simple_booleans
     env[:simple_strings] = simple_strings.map { |arg| arg.split "=" }.map { |k,v| [k.to_sym, v] }.to_h
     env[:simple_booleans] = simple_booleans.map { |arg| [arg[1..-1], arg[0]=="+"] }.map { |k,v| [k.to_sym, v] }.to_h
   end
