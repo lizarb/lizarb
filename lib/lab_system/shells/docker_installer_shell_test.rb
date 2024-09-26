4c86b4c8a2ce5ec230caae42e6e62141ab000d6f
Thiago Almeida <thiago@lizarb.org>
2024-09-26 01:04:22 +0000

    LabSystem DockerInstallerShellTest - removing dummy tests

--- lib/lab_system/shells/docker_installer_shell_test.rb
+++ lib/lab_system/shells/docker_installer_shell_test.rb
@@ -9,18 +9,4 @@ class LabSystem::DockerInstallerShellTest < DevSystem::ShellTest
   
   # 
 
-  test :subject_class, :sum do
-    a = subject_class.sum 1, 2
-    b = 3
-    assert_equality a, b
-  end
-  
-  # 
-
-  test :subject_class, :sub do
-    a = subject_class.sub 1, 2
-    b = -1
-    assert_equality a, b
-  end
-  
 end
