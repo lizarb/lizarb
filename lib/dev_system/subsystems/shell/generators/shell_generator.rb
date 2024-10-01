94a92401d98164efdcd8feb44bca8ee50dbccd34
Thiago Almeida <thiago@lizarb.org>
2024-10-01 05:24:41 +0000

    DevSystem ShellGenerator - updating all views

--- lib/dev_system/subsystems/shell/generators/shell_generator.rb
+++ lib/dev_system/subsystems/shell/generators/shell_generator.rb
@@ -9,12 +9,16 @@ class DevSystem::ShellGenerator < DevSystem::SimpleGenerator
     place!
 
     create_controller @name, @controller_class, @place, @path do |unit, test|
-      unit.section :controller_section_1, caption: "#{@class_name}.call('sum', 1, 2)"
-      unit.section :controller_section_2, caption: "#{@class_name}.sum(1, 2)"
-      unit.section :controller_section_3, caption: "#{@class_name}.sub(1, 2)"
+      unit.section :controller_section_1, caption: "#{@class_name}.call({action: \"list\"})"
+      unit.section :controller_section_2, caption: "#{@class_name}._call_sub_method_a({})"
+      unit.section :controller_section_3, caption: "#{@class_name}._call_sub_method_b({})"
+      unit.section :controller_section_list,   caption: "#{@class_name}.call_list({})"
+      unit.section :controller_section_create, caption: "#{@class_name}.call_create({})"
+      unit.section :controller_section_read,   caption: "#{@class_name}.call_read({})"
+      unit.section :controller_section_update, caption: "#{@class_name}.call_update({})"
+      unit.section :controller_section_delete, caption: "#{@class_name}.call_delete({})"
+
       test.section :controller_test_section_1
-      test.section :controller_test_section_2
-      test.section :controller_test_section_3
     end
   end
 
@@ -30,39 +34,70 @@ __END__
 
 # view controller_section_1.rb.erb
 
-  def self.call(a, b, c)
-    return sum(b, c) if a == "sum"
-    return sub(b, c) if a == "sub"
-    
-    raise Error, "invalid a: #{a}"
+  def self.call(env)
+    super
+
+    method_name = "call_#{env[:action]}"
+    if respond_to? method_name
+      # before
+      _call_sub_method_a env
+
+      # action
+      public_send method_name, env
+
+      # after
+      _call_sub_method_b env
+      return
+    end
+
+    log "action not found: #{method_name.inspect}"
+    raise NoMethodError, "action not found: #{method_name.inspect}"
   end
+
 # view controller_section_2.rb.erb
   
-    def self.sum(a, b)
-      a + b
-    end
+  def self._call_sub_method_a(env)
+    # your code here
+  end
 # view controller_section_3.rb.erb
 
-  def self.sub(a, b)
-    a - b
+  def self._call_sub_method_b(env)
+    # your code here
   end
+
+# view controller_section_list.rb.erb
+
+  def self.call_list(env)
+    # your code here
+  end
+
+# view controller_section_create.rb.erb
+
+  def self.call_create(env)
+    # your code here
+  end
+
+# view controller_section_read.rb.erb
+
+  def self.call_read(env)
+    # your code here
+  end
+
+# view controller_section_update.rb.erb
+
+  def self.call_update(env)
+    # your code here
+  end
+
+# view controller_section_delete.rb.erb
+
+  def self.call_delete(env)
+    # your code here
+  end
+
 # view controller_test_section_1.rb.erb
 
   test :subject_class, :subject do
     assert_equality <%= @class_name %>, subject_class
     assert_equality <%= @class_name %>, subject.class
-  end
-# view controller_test_section_2.rb.erb
-
-  test :subject_class, :sum do
-    a = subject_class.sum 1, 2
-    b = 3
-    assert_equality a, b
-  end
-# view controller_test_section_3.rb.erb
-
-  test :subject_class, :sub do
-    a = subject_class.sub 1, 2
-    b = -1
-    assert_equality a, b
   end
