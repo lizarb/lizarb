6dffbab77ccd49381ae6864e6dec5be49fd763b6
Thiago Almeida <thiago@lizarb.org>
2024-10-13 06:56:39 +0000

    DevSystem LogCommand - action levels becomes method print_log_levels

--- lib/dev_system/subsystems/log/commands/log_command.rb
+++ lib/dev_system/subsystems/log/commands/log_command.rb
@@ -3,16 +3,21 @@ class DevSystem::LogCommand < DevSystem::SimpleCommand
   # liza log
 
   def call_default
-    log :higher, "args = #{args}"
-
-    DevBox[:log].handlers.each do |key, log_class|
-      log "handler #{key} maps to #{log_class}"
-    end
+    print_log_handlers
+    print_log_levels
   end
-  
-  # liza log:levels
 
-  def call_levels
+  def print_log_handlers
+    puts stick " LOG HANDLERS ".center(80, "-"), :b, system.color
+    puts
+    DevBox[:log].handlers.each do |key, log_class|
+      log "handler :#{key.to_s.ljust_blanks 25} => #{log_class}"
+    end
+    puts
+  end
+
+  def print_log_levels
+    puts stick " LOG LEVELS ".center(80, "-"), :b, system.color
     puts
     log :highest, ":highest #{ log_levels[ :highest ] } >= #{ log_level }"
     log :higher,  ":higher  #{ log_levels[ :higher  ] } >= #{ log_level }"
@@ -21,6 +26,7 @@ class DevSystem::LogCommand < DevSystem::SimpleCommand
     log :low,     ":low     #{ log_levels[ :low     ] } >= #{ log_level }"
     log :lower,   ":lower   #{ log_levels[ :lower   ] } >= #{ log_level }"
     log :lowest,  ":lowest  #{ log_levels[ :lowest  ] } >= #{ log_level }"
+    puts
   end
 
 end
