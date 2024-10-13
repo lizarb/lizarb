e6e43e79e7188fa47f2892f56849a9ff1d60c62f
Thiago Almeida <thiago@lizarb.org>
2024-10-13 06:37:31 +0000

    DevSystem FormatCommand - action help becomes method help

--- lib/dev_system/subsystems/shell/commands/format_command.rb
+++ lib/dev_system/subsystems/shell/commands/format_command.rb
@@ -5,7 +5,7 @@ class DevSystem::FormatCommand < DevSystem::SimpleCommand
   def call_default
     log :higher, "args = #{args.inspect}"
 
-    return call_help if simple_args.count < 2
+    return help if simple_args.count < 2
 
     format = simple_arg(0).to_sym
     fname = simple_arg(1)
@@ -24,7 +24,7 @@ class DevSystem::FormatCommand < DevSystem::SimpleCommand
     TextShell.write fname, content
   end
 
-  def call_help
+  def help
     puts
     puts "Usage: liza format FORMAT FILENAME"
     puts "  FORMAT - format name (#{ valid_formats.join(", ") })"
