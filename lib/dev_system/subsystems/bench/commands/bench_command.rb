52afd7fa130e6e6e1381c169ba361f267ea814ba
Thiago Almeida <thiago@lizarb.org>
2024-11-06 00:39:36 +0000

    DevSystem BenchCommand - before

--- lib/dev_system/subsystems/bench/commands/bench_command.rb
+++ lib/dev_system/subsystems/bench/commands/bench_command.rb
@@ -1,5 +1,12 @@
 class DevSystem::BenchCommand < DevSystem::SimpleCommand
 
+  def before
+    super
+    log "simple_args     #{ simple_args }"
+    log "simple_booleans #{ simple_booleans }"
+    log "simple_strings  #{ simple_strings }"
+  end
+
   # liza bench NAME
   def call_default
     log :higher, "env.count is #{env.count}"
