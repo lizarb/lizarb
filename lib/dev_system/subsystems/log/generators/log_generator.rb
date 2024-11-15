5607403024284d661699f59f7d066d4f8be91a3a
Thiago Almeida <thiago@lizarb.org>
2024-11-15 21:20:01 +0000

    DevSystem LogGenerator - refactored to work with ControllerGenerator

--- lib/dev_system/subsystems/log/generators/log_generator.rb
+++ lib/dev_system/subsystems/log/generators/log_generator.rb
@@ -1,85 +1,52 @@
-class DevSystem::LogGenerator < DevSystem::SimpleGenerator
+class DevSystem::LogGenerator < DevSystem::ControllerGenerator
+  
+  section :actions
   
   # liza g log name place=app
 
   def call_default
-    @controller_class = Log
+    set_default_super ""
 
-    name!
-    place!
-
-    create_controller @name, @controller_class, @place, @path do |unit, test|
-      unit.section :controller_section_1
-      test.section :controller_test_section_1
+    create_controller do |unit, test|
+      unit.section name: :controller_section_1, skip_section: true
+      test.section name: :controller_test_section_1, skip_section: true
     end
   end
   
   # liza g log:handler name place=app
 
   def call_handler
-    @controller_class = HandlerLog
-
-    name!
-    place!
+    set_default_super "handler"
     append_handler_to_log_panel
 
-    create_controller @name, @controller_class, @place, @path do |unit, test|
-      unit.section :handler_section_1
-      test.section :handler_test_section_1
+    create_controller do |unit, test|
+      unit.section name: :handler_section_1, skip_section: true
+      test.section name: :handler_test_section_1, skip_section: true
     end
   end
 
   def append_handler_to_log_panel
-    file = TextFileShell.new App.path.join("dev_box.rb")
-    
-    lines = LineShell.extract_wall_of file.old_lines, "    handler :"
-    lines << "    handler :#{@name}\n"
-    file.new_lines = LineShell.replace_wall_of file.old_lines, "    handler :", lines
+    path = App.directory.join "dev_box.rb"
 
-    add_change file
+    contents = read_file path
+    old_lines = contents.split("\n")
+
+    lines = LineShell.extract_wall_of old_lines, "    handler :"
+    lines << "    handler :#{controller_name}"
+    lines = LineShell.replace_wall_of old_lines, "    handler :", lines
+
+    contents = lines.join("\n")
+    update_file path, contents
   end
 
   # liza g log:examples
 
   def call_examples
-    # TODO: append once per example
-    @name = "logger"
+    # This bypasses the name check
+    @controller_name = "logger"
     append_handler_to_log_panel
     #
     copy_examples Log
   end
 
 end
-
-__END__
-
-# view controller_section_1.rb.erb
-
-  def self.call(env)
-    super
-    env[:object_parsed] = env[:object].to_s
-    true
-  end
-
-# view controller_test_section_1.rb.erb
-
-  test :subject_class, :subject do
-    assert_equality <%= @class_name %>, subject_class
-    assert_equality <%= @class_name %>, subject.class
-  end
-
-# view handler_section_1.rb.erb
-
-  # TODO: https://rubyapi.org/3.1/o/logger
-  def self.call(env)
-    super
-    Kernel.puts env[:object_parsed]
-    true
-  end
-
-# view handler_test_section_1.rb.erb
-
-  test :subject_class, :subject do
-    assert_equality <%= @class_name %>, subject_class
-    assert_equality <%= @class_name %>, subject.class
-  end
--- lib/dev_system/subsystems/log/generators/log_generator_test.rb
+++ lib/dev_system/subsystems/log/generators/log_generator_test.rb
@@ -3,8 +3,15 @@ class DevSystem::LogGeneratorTest < DevSystem::SimpleGeneratorTest
   #
   
   test :subject do
-    assert_equality DevSystem::LogGenerator, subject_class
-    assert_equality DevSystem::LogGenerator, subject.class
+    assert_equality subject_class, DevSystem::LogGenerator
+    assert_equality subject.class, DevSystem::LogGenerator
   end
+  
+  test_erbs_defined(
+    "controller_section_1.rb.erb",
+    "controller_test_section_1.rb.erb",
+    "handler_section_1.rb.erb",
+    "handler_test_section_1.rb.erb"
+  )
   
 end
