217279fd67698fa775437ed212f840e9e63e7c8a
Thiago Almeida <thiago@lizarb.org>
2024-11-15 21:19:10 +0000

    DevSystem CommandGenerator - refactored to work with ControllerGenerator

--- lib/dev_system/subsystems/command/generators/command_generator.rb
+++ lib/dev_system/subsystems/command/generators/command_generator.rb
@@ -1,4 +1,12 @@
-class DevSystem::CommandGenerator < DevSystem::SimpleGenerator
+class DevSystem::CommandGenerator < DevSystem::ControllerGenerator
+
+  section :default
+  
+  def arg_action_names
+    ["default", *super]
+  end
+
+  section :actions
 
   # liza g command name place=app
 
@@ -9,47 +17,30 @@ class DevSystem::CommandGenerator < DevSystem::SimpleGenerator
   # liza g command:simple name place=app
 
   def call_simple
-    @controller_class = Command
-
-    name!
-    place!
-    @args = command.simple_args_from_2
-
-    ancestor = SimpleCommand
-    create_controller @name, @controller_class, @place, @path, ancestor: ancestor do |unit, test|
-      unit.section :simple_command_section_1,
-        caption: "filters"
-
-      unit.section :simple_command_section_2,
-        caption: "liza #{ @name } k1=v1 k3=v3 sa se si +a +b -c -d",
-        method_name: "default"
+    set_default_super "simple"
+    
+    create_controller do |unit, test|
+      unit.section name: :filters, render_key: :section_simple_filters
+      unit.section name: :actions, render_key: :section_simple_actions
       
-      @args.each do |arg|
-        unit.section :simple_command_section_3,
-          caption: "liza #{ @name }:#{ arg }",
-          method_name: arg
+      arg_action_names.each do |action_name|
+        unit.view name: action_name, render_key: :view_simple, format: :txt
       end
 
-      test.section :controller_test_section_1
+      test.section name: :subject
     end
   end
 
   # liza g command:base name place=app
 
   def call_base
-    @controller_class = Command
-
-    name!
-    place!
-    @args = command.simple_args_from_2
-
-    ancestor = BaseCommand
-    create_controller @name, @controller_class, @place, @path, ancestor: ancestor do |unit, test|
-      unit.section :base_command_section_1,
-        caption: "liza #{ @name }"
-      
-      test.section :controller_test_section_1
+    set_default_super "base"
+    
+    create_controller do |unit, test|
+      unit.section name: :section_base, skip_section: true
+      test.section name: :subject
     end
+    
   end
   
   # liza g command:examples
@@ -59,50 +50,3 @@ class DevSystem::CommandGenerator < DevSystem::SimpleGenerator
   end
   
 end
-
-__END__
-
-# view simple_command_section_1.rb.erb
-
-  def before
-    super
-    @t = Time.now
-    log "simple_args     #{ simple_args }"
-    log "simple_strings  #{ simple_strings }"
-    log "simple_booleans #{ simple_booleans }"
-  end
-  
-  def after
-    super
-    log "#{ @t.diff }s | done"
-  end
-# view simple_command_section_2.rb.erb
-
-  def call_<%= @current_section[:method_name] %>
-    log stick :b, :<%= ColorShell.colors.keys.sample %>, "I just think Ruby is the Best for coding!"
-<% @args.each do |arg| -%>
-    call_<%= arg %>
-<% end -%>
-  end
-# view simple_command_section_3.rb.erb
-
-  def call_<%= @current_section[:method_name] %>
-    log stick :b, :<%= ColorShell.colors.keys.sample %>, "I just think Ruby is the Best for coding!"
-  end
-# view base_command_section_1.rb.erb
-
-  def self.call(env)
-    super
-    log "args is #{ args.inspect }"
-
-    log stick :b, :<%= ColorShell.colors.keys.sample %>, "I just think Ruby is the Best for coding!"
-
-    log "done at #{ Time.now }"
-  end
-
-# view controller_test_section_1.rb.erb
-
-  test :subject_class, :subject do
-    assert_equality <%= @class_name %>, subject_class
-    assert_equality <%= @class_name %>, subject.class
-  end
--- lib/dev_system/subsystems/command/generators/command_generator_test.rb
+++ lib/dev_system/subsystems/command/generators/command_generator_test.rb
@@ -1,7 +1,15 @@
 class DevSystem::CommandGeneratorTest < DevSystem::SimpleGeneratorTest
 
   test :subject_class do
-    assert subject_class == DevSystem::CommandGenerator
+    assert_equality subject_class, DevSystem::CommandGenerator
+    assert_equality subject.class, DevSystem::CommandGenerator
   end
+
+  test_erbs_defined(
+    "section_base.rb.erb",
+    "section_simple_actions.rb.erb",
+    "section_simple_filters.rb.erb",
+    "view_simple.txt.erb"
+  )
 
 end
