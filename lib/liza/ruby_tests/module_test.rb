c02d5bae98a4e5613a843c5306e062ceb46d1409
Thiago Almeida <thiago@lizarb.org>
2024-11-16 00:01:35 +0000

    Liza ModuleTest - fixing tests

--- lib/liza/ruby_tests/module_test.rb
+++ lib/liza/ruby_tests/module_test.rb
@@ -1,18 +1,18 @@
 class Liza::ModuleTest < Liza::ObjectTest
   
   test :subject_class do
-    assert subject_class == Module
+    assert_equality subject_class, Module
   end
 
   test :source_location do
-    assert Object.source_location == []
+    assert_equality Object.source_location, []
 
-    assert Liza::Command.source_location[1] == 1
-    assert IrbCommand.source_location[1] == 2
+    assert_equality Liza::Command.source_location[1], 1
+    assert_equality IrbCommand.source_location[1], 1
   end
 
   test :source_location_radical do
-    assert Object.source_location_radical == nil
+    assert_equality Object.source_location_radical, nil
 
     assert Liza::Command.source_location_radical.end_with? "/lib/dev_system/subsystems/command/command"
     assert IrbCommand.source_location_radical.end_with? "/lib/dev_system/subsystems/command/commands/irb_command"
