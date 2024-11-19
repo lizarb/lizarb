4211625f8b85f98a4c61b3c7e0aa07f3d9247f46
Thiago Almeida <thiago@lizarb.org>
2024-11-19 04:46:32 +0000

    DevSystem KernelShell - improved logging

--- lib/dev_system/subsystems/shell/shells/kernel_shell.rb
+++ lib/dev_system/subsystems/shell/shells/kernel_shell.rb
@@ -3,20 +3,24 @@ class DevSystem::KernelShell < DevSystem::Shell
   # 
 
   def self.call_backticks(command, log_level: :normal)
-    log log_level, "#{stick command, DevSystem.color} | executing and outputting result"
+    t = Time.now
+    log log_level, "#{stick command, DevSystem.color} | returns a string | executing..."
     result = Kernel.send :`, command
+  ensure
+    log log_level, "#{t.diff}s | executed with an output of #{"#{result.size} bytes" if result}#{result.inspect unless result} |"
     puts result if log? log_level
-    result
   end
 
   #
 
   def self.call_system(command, log_level: :normal)
-    log log_level, "#{command} | executing"
+    t = Time.now
+    log log_level, "#{command} | returns a boolean | executing..."
     success = Kernel.system(command)
-    message = success ? (stick :green, "successfully") : (stick :red, "failed")
-    log log_level, "Command executed #{message}"
     success
+  ensure
+    message = success ? (stick :green, "successfully") : (stick :red, "failed")
+    log log_level, "#{t.diff}s | #{message}"
   end
 
 end
