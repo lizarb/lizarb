bdb61057c79ad78125d239bdd56cf70e05511298
Thiago Almeida <thiago@lizarb.org>
2024-11-20 06:42:20 +0000

    DevSystem NotFoundGeneratorTest - added tests

--- lib/dev_system/subsystems/generator/generators/not_found_generator_test.rb
+++ lib/dev_system/subsystems/generator/generators/not_found_generator_test.rb
@@ -1,7 +1,14 @@
 class DevSystem::NotFoundGeneratorTest < DevSystem::SimpleGeneratorTest
 
-  test :subject_class do
-    assert subject_class == DevSystem::NotFoundGenerator
+  test :subject_class, :subject do
+    assert_equality subject_class, DevSystem::NotFoundGenerator
+    assert_equality subject.class, DevSystem::NotFoundGenerator
+  end
+
+  test :call_default do
+    assert_no_raise do
+      subject.call_default
+    end
   end
 
 end
