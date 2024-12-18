0423267b41fbe4ae1b0cda228d39c4d1053ec74b
Thiago Almeida <thiago@lizarb.org>
2024-12-18 23:00:13 +0000

    DevSystem TestCommand - improved totals readability

--- lib/dev_system/commands/test_command.rb
+++ lib/dev_system/commands/test_command.rb
@@ -144,7 +144,17 @@ class DevSystem::TestCommand < DevSystem::Command
       Liza.log "#{ColorShell.color_unit test_class.subject_class}#{" " * size} #{total_line}"
     end
     puts
-    Liza.log "#{"Total".ljust 60} #{totals}"
+    totals_line = totals.map do |k, v|
+      s = "#{k}: #{v}"
+      s = ":#{k} => #{v.to_s.rjust 2}"
+      s = stick :b, :white, :light_red, s if k==:errors && v.positive?
+      s = stick :b, :black, :light_yellow, s if k==:fails && v.positive?
+      s = stick :b, :white, :light_blue, s if k==:todos && v.positive?
+      s = stick :b, :black, :light_green, s if k==:passes && v.positive?
+      s
+    end
+    totals_line = "{ #{ totals_line.join(", ") } }"
+    Liza.log "#{"Total".ljust 60} #{totals_line}"
     puts
   end
 
