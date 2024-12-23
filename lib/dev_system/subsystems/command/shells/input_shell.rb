00e2f7482d9f0e191c0e14a5bcf14c162c8f0bf8
Thiago Almeida <thiago@lizarb.org>
2024-12-23 22:41:01 +0000

    DevSystem InputShell - rescue_input_interrupt

--- lib/dev_system/subsystems/command/shells/input_shell.rb
+++ lib/dev_system/subsystems/command/shells/input_shell.rb
@@ -3,8 +3,6 @@ class DevSystem::InputShell < DevSystem::Shell
   # Liza lazily requires gems required in Controllers
   require "tty-prompt"
 
-  section :action_ask
-
   def self.prompt
     call({})
     @prompt ||= TTY::Prompt.new symbols: {marker: ">", radio_on: "x", radio_off: " "}
@@ -12,11 +10,8 @@ class DevSystem::InputShell < DevSystem::Shell
 
   def self.pick_one title, options = ["Yes", "No"]
     prompt.select title, options, filter: true, show_help: :always, per_page: 20
-  rescue TTY::Reader::InputInterrupt
-    puts
-    puts
-    log "Control-C"
-    exit
+  rescue Exception => e
+    rescue_input_interrupt e
   end
 
   def self.pick_color title = "Pick a color", string: nil
@@ -26,15 +21,12 @@ class DevSystem::InputShell < DevSystem::Shell
       title,
       options,
       filter: true, show_help: :always, per_page: 28
-  rescue TTY::Reader::InputInterrupt
-    puts
-    puts
-    log "Control-C"
-    exit
+  rescue Exception => e
+    rescue_input_interrupt e
   end
 
   def self.multi_select title, choices, selected: :all
-    raise "choices must be a hash" unless choices.is_a? Hash
+    raise ArgumentError, "choices must be a hash", caller unless choices.is_a? Hash
     return choices if choices.empty?
     
     default = 1..choices.count if selected == :all
@@ -49,7 +41,13 @@ class DevSystem::InputShell < DevSystem::Shell
       default: default
     }
     prompt.multi_select title, choices, options
-  rescue TTY::Reader::InputInterrupt
+  rescue Exception => e
+    rescue_input_interrupt e
+  end
+
+  def self.rescue_input_interrupt(e)
+    log :higher, "#{e.class}: #{e.message}"
+    raise e unless e.is_a? TTY::Reader::InputInterrupt
     puts
     puts
     log "Control-C"
