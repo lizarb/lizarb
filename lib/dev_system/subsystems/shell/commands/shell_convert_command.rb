3ed3254fc5b1146221efce5c89597ff35d9bc592
Thiago Almeida <thiago@lizarb.org>
2024-12-24 05:38:03 +0000

    DevSystem ShellConvertCommand - refactored

--- lib/dev_system/subsystems/shell/commands/shell_convert_command.rb
+++ lib/dev_system/subsystems/shell/commands/shell_convert_command.rb
@@ -1,45 +1,48 @@
 class DevSystem::ShellConvertCommand < DevSystem::SimpleCommand
   
-  # liza shell_convert FORMAT FILENAME
+  # liza shell_convert FORMAT FILENAMES
 
   def call_default
-    log :higher, "args = #{args.inspect}"
+    valid_converters = Shell.panel.converters.keys
+    color = system.color
 
-    return help if simple_args.count < 2
+    log :normal, (stick color, "valid converters are #{valid_converters.map { stick :black, color, :b, _1.to_s }.map(&:to_s).join ', '}")
+
+    set_input_arg 0 do |default|
+      title = "Which converter are we going to use?"
+      answer = InputShell.pick_one title, valid_converters
+    end
 
     format = simple_arg(0).to_sym
-    fname = simple_arg(1)
-    content = TextShell.read fname
-    log "IN:"
-    puts content if log? :normal
+    log :normal, (stick color, "converter is #{format}")
 
-    format_to = DevBox[:shell].converters[format][:to]
-    fname = "#{fname}.#{format_to}"
-    convert_env = {format: format, convert_in: content}
-    DevBox.convert convert_env
-    DevBox.convert convert_env
-    DevBox.convert convert_env
-    content = convert_env[:convert_out]
+    set_input_arg 1 do |default|
+      title = "Which files are we going to convert?"
+      local_files = Dir["*.#{format}"]
+      choices = local_files.map { [_1, _1] }.to_h
+      selected = [choices.keys.first]
+      answers = InputShell.multi_select title, choices, selected: selected
+      answers.join(",")
+    end
 
-    log "OUT:"
-    puts content if log? :normal
+    fnames = simple_arg(1).to_s.split(",")
+    log :normal, (stick color, "selected files are #{fnames.join ', '}")
+    fnames.each do |fname|
+      content = TextShell.read fname
+      log :higher, (stick color, "IN:")
+      puts content if log? :higher
 
-    TextShell.write fname, content
+      format_to = DevBox[:shell].converters[format][:to]
+      fname = "#{fname}.#{format_to}"
+      convert_env = {format: format, convert_in: content}
+      DevBox.convert convert_env
+
+      content = convert_env[:convert_out]
+      log :higher, (stick color, "OUT:")
+      puts content if log? :higher
+
+      TextShell.write fname, content
+    end
   end
 
-  def help
-    puts
-    puts "Usage: liza convert FORMAT FILENAME"
-    puts "  FORMAT - format name (#{ valid_formats.join(", ") })"
-    puts "  FILENAME - file name (a.haml)"
-    puts
-  end
-
-  private
-
-  def valid_formats
-    DevBox[:shell].converters.keys
-  end
-
-  
 end
