49b49df084f755baeba583fa57fe915a9a654e1b
Thiago Almeida <thiago@lizarb.org>
2025-01-05 08:15:34 +0000

    DevSystem AppShellTest - fixing tests

--- lib/dev_system/subsystems/shell/shells/app_shell_test.rb
+++ lib/dev_system/subsystems/shell/shells/app_shell_test.rb
@@ -6,12 +6,12 @@ class DevSystem::AppShellTest < DevSystem::ShellTest
 
   test :writable_systems do
     a = subject_class.writable_systems.keys
-    b = [:dev, :happy, :net, :web, :work, :micro, :desk, :crypto, :media, :art, :deep, :lab]
+    b = [:dev, :happy, :net, :web, :work, :micro, :desk, :crypto, :media, :art, :deep, :prime, :lab, :eco]
     assert_equality a, b
   end
 
   test :subject do
-    system_keys = ["dev", "happy", "net", "web", "work", "micro", "desk", "crypto", "media", "art", "deep", "lab"]
+    system_keys = ["dev", "happy", "net", "web", "work", "micro", "desk", "crypto", "media", "art", "deep", "prime", "lab", "eco"]
     assert_equality subject.consts.keys,           [:top_level, :liza, :systems, :app]
     assert_equality subject.consts[:top_level],    [Lizarb, App, Liza]
     assert_equality subject.consts[:liza].keys,    ["unit", "helper_units", "systemic_units", "subsystemic_units", "ruby_tests"]
