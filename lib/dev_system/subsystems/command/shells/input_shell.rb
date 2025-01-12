f9ae9d750b3b3eb380f51964c68de2127af03192
Thiago Almeida <thiago@lizarb.org>
2025-01-12 23:49:50 +0000

    DevSystem InputShell - small fix

--- lib/dev_system/subsystems/command/shells/input_shell.rb
+++ lib/dev_system/subsystems/command/shells/input_shell.rb
@@ -63,7 +63,7 @@ class DevSystem::InputShell < DevSystem::Shell
     
     default = 1..choices.count if selected == :all
     default = [] if selected == :none
-    default = selected.map { choices.keys.index(_1) + 1 } if selected.is_a? Array
+    default = selected.map { choices.values.index(_1) + 1 } if selected.is_a? Array
 
     options = {
       enum: ")",
