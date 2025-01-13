63211cf898b94c826d695b9048c9f35117eeb540
Thiago Almeida <thiago@lizarb.org>
2025-01-13 19:37:12 +0000

    DevSystem TestCommand - replacing a filtering strategy

--- lib/dev_system/commands/test_command.rb
+++ lib/dev_system/commands/test_command.rb
@@ -1,33 +1,48 @@
 class DevSystem::TestCommand < DevSystem::SimpleCommand
 
+  def before
+    super
+
+    set_default_array :domains, AppShell.get_writable_domains.keys
+    set_default_boolean :run, true
+    
+    set_input_array :domains do |default|
+      domains = AppShell.get_writable_domains
+      title = "Which domains are we going to test?"
+      InputShell.pick_domains domains, default, title
+    end
+  end
+
   def call_default
     Lizarb.eager_load!
     DevBox[:log].sidebar_size 60
 
-    args = env[:args]
-    log "args = #{args.inspect}"
-
     now = Time.now
-    test_classes = _get_test_classes
 
-    if args.any?
-      test_classes = test_classes.select { |tc| args.any? { tc.source_location_radical.snakecase.include? _1.snakecase } }
+    app_shell = AppShell.new
+    app_shell.filter_by_unit Liza::UnitTest
+
+    domains = simple_array(:domains)
+    log "domains = #{domains}"
+    app_shell.filter_by_domains domains
+
+    name = simple_args[0]
+    app_shell.filter_by_starting_with name if name
+
+    set_input_boolean :run do |default|
+      InputShell.yes? "Do you want to run the tests?", default: default
     end
 
-    _call_silence_other_units
-    
-    if Lizarb.is_app_dir
-      test_classes = test_classes.select { |tc| tc.source_location[0].include? Lizarb.app_dir.to_s }
-    end
-
+    should_run = simple_boolean(:run)
+    test_classes = app_shell.lists.flatten
     log "Testing #{test_classes}"
-    _call_testing test_classes
+    _call_testing test_classes if should_run
     log "Done Testing (#{now.diff}s)"
 
     puts
 
     log "Counting #{test_classes.count} Test Classes"
-    _call_counting test_classes
+    _call_counting app_shell
     log "Done Counting (#{now.diff}s)"
   end
 
@@ -48,49 +63,6 @@ class DevSystem::TestCommand < DevSystem::SimpleCommand
     end
   end
 
-  def _get_test_classes
-    ret = []
-
-    only_tests = proc { |klasses| klasses.select { _1 < Liza::Test } }
-
-    list = AppShell.consts[:liza].to_a
-    list = [list[-1]] + list[0..-2]
-    list[0][1].tap do
-      _1.delete ObjectTest
-      _1.delete AppTest
-      list[0][1] = [ObjectTest, *_1, AppTest]
-    end
-    list.each do |category, klasses|
-      ret += only_tests.call klasses
-    end
-
-    AppShell.consts[:systems].each do |system_name, tree_system|
-      ret += only_tests.call tree_system["system"]
-      ret += only_tests.call tree_system["box"]
-      tree_system["controllers"].each do |family, klasses|
-        ret += only_tests.call klasses
-      end
-
-      tree_system["subsystems"].each do |subsystem, tree_subsystem|
-        ret += only_tests.call tree_subsystem["panel"]
-        ret += only_tests.call tree_subsystem["controller"]
-        tree_subsystem["controllers"].each do |controller_class, klasses|
-          ret += only_tests.call klasses
-        end
-      end
-    end
-
-    AppShell.consts[:app].each do |system_name, tree_system|
-      tree_system["controllers"].each do |family, structure|
-        structure.each do |division, klasses|
-          ret += only_tests.call klasses
-        end
-      end
-    end
-
-    ret
-  end
-
   def _call_testing test_classes
     i, count = 0, test_classes.count
     for test_class in test_classes
@@ -98,49 +70,57 @@ class DevSystem::TestCommand < DevSystem::SimpleCommand
     end
   end
 
-
-  def _call_counting test_classes
+  def _call_counting(app_shell)
     puts
     Liza.log ""
-    Liza.log stick :b, " TEST TOTALS ".center(140, "-")
+    Liza.log stick :b, " TEST TOTALS ".center(120, "-")
     Liza.log ""
     puts
     totals = Hash.new { 0 }
     last_namespace = nil
-    test_classes.each do |test_class|
-      namespace = test_class.first_namespace
-      if last_namespace != namespace
-        puts
-        s = namespace.length.positive? ? namespace : "App"
-        Liza.log stick :b, " #{s} ".center(140, "-")
-        puts
-      end
-      last_namespace = namespace
+    app_shell.get_domains.each do |domain|
+      next if domain.empty?
+      puts
+      puts typo.h1 domain.name.to_s, domain.color, length: 120
+      puts
 
-      test_class.totals.each do |k, v|
-        totals[k] += v.size
+      domain.layers.each do |layer|
+        next if layer.objects.empty?
+
+        m = "h#{layer.level}"
+        puts typo.send m, layer.name.to_s, layer.color, length: 120 unless layer.level == 1
+
+        puts stick layer.color, layer.path
+        puts
+
+        layer.objects.each do |object|
+          object.totals.each do |k, v|
+            totals[k] += v.size
+          end
+          size = 60 - object.to_s.size
+          total_line = object.totals.map do |k, v|
+            s = "#{k}: #{v.size}"
+            s = ":#{k} => #{v.size.to_s.rjust 2}"
+            s = stick :b, :white, :light_red, s    if k==:errors && v.size.positive?
+            s = stick :b, :black, :light_yellow, s if k==:fails  && v.size.positive?
+            s = stick :b, :white, :light_blue, s   if k==:todos  && v.size.positive?
+            s = stick :b, :light_green, s          if k==:passes && v.size.positive?
+            s
+          end
+          total_line = "{ #{ total_line.join(", ") } }"
+          Liza.log "#{ColorShell.color_unit object}#{" " * size} #{total_line}"
+        end
+        puts
       end
-      size = 60 - test_class.subject_class.to_s.size
-      total_line = test_class.totals.map do |k, v|
-        s = "#{k}: #{v.size}"
-        s = ":#{k} => #{v.size.to_s.rjust 2}"
-        s = stick :b, :white, :light_red, s if k==:errors && v.size.positive?
-        s = stick :b, :black, :light_yellow, s if k==:fails && v.size.positive?
-        s = stick :b, :white, :light_blue, s if k==:todos && v.size.positive?
-        s = stick :b, :light_green, s if k==:passes && v.size.positive?
-        s
-      end
-      total_line = "{ #{ total_line.join(", ") } }"
-      Liza.log "#{ColorShell.color_unit test_class.subject_class}#{" " * size} #{total_line}"
     end
     puts
     totals_line = totals.map do |k, v|
       s = "#{k}: #{v}"
       s = ":#{k} => #{v.to_s.rjust 2}"
-      s = stick :b, :white, :light_red, s if k==:errors && v.positive?
-      s = stick :b, :black, :light_yellow, s if k==:fails && v.positive?
-      s = stick :b, :white, :light_blue, s if k==:todos && v.positive?
-      s = stick :b, :black, :light_green, s if k==:passes && v.positive?
+      s = stick :b, :white, :light_red, s    if k==:errors && v.positive?
+      s = stick :b, :black, :light_yellow, s if k==:fails  && v.positive?
+      s = stick :b, :white, :light_blue, s   if k==:todos  && v.positive?
+      s = stick :b, :black, :light_green, s  if k==:passes && v.positive?
       s
     end
     totals_line = "{ #{ totals_line.join(", ") } }"
