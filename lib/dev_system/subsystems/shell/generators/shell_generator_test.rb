9c69b221773891004bf82667b4a070e0b0aaedc5
Thiago Almeida <thiago@lizarb.org>
2025-02-20 18:20:23 +0000

    DevSystem ShellGeneratortest - small fix

--- lib/dev_system/subsystems/shell/generators/shell_generator_test.rb
+++ lib/dev_system/subsystems/shell/generators/shell_generator_test.rb
@@ -9,8 +9,11 @@ class DevSystem::ShellGeneratorTest < DevSystem::SimpleGeneratorTest
   
   test_erbs_defined(
     "actions.rb.erb",
+    "converter.rb.erb",
+    "formatter.rb.erb", 
     "helpers.rb.erb",
     "instance.rb.erb",
+    "renderer.rb.erb",
     "test.rb.erb",
     "view_simple.txt.erb"
   )
