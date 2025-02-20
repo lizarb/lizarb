e4b0450af8d9ba32d1bd63141a1ae8ae235c1ee3
Thiago Almeida <thiago@lizarb.org>
2025-02-20 19:17:23 +0000

    DevSystem BenchPanelTest - fixing tests

--- lib/dev_system/subsystems/bench/bench_panel_test.rb
+++ lib/dev_system/subsystems/bench/bench_panel_test.rb
@@ -17,39 +17,39 @@ class DevSystem::BenchPanelTest < Liza::PanelTest
   
   def forge_with(args)
     command = SimpleCommand.new
-    command_env = {command: , args: }
-    command.instance_variable_set :@env, command_env
-    env = subject.forge command_env
-    subject.forge_shortcut env
-    env
+    command_menv = {command: , args: }
+    command.instance_variable_set :@menv, command_menv
+    menv = subject.forge command_menv
+    subject.forge_shortcut menv
+    menv
   end
   
   test :forge, true do
-    env = forge_with ["objects"]
-    assert_equality env[:bench_name_original], "objects"
-    assert_equality env[:bench_name], "objects"
-    assert_equality env[:bench_action_original], nil
+    menv = forge_with ["objects"]
+    assert_equality menv[:bench_name_original], "objects"
+    assert_equality menv[:bench_name], "objects"
+    assert_equality menv[:bench_action_original], nil
   end
   
   test :forge, true, :action do
-    env = forge_with ["objects:quadratic"]
-    assert_equality env[:bench_name_original], "objects"
-    assert_equality env[:bench_name], "objects"
-    assert_equality env[:bench_action_original], "quadratic"
+    menv = forge_with ["objects:quadratic"]
+    assert_equality menv[:bench_name_original], "objects"
+    assert_equality menv[:bench_name], "objects"
+    assert_equality menv[:bench_action_original], "quadratic"
   end
   
   test :forge, false do
-    env = forge_with ["x"]
-    assert_equality env[:bench_name_original], "x"
-    assert_equality env[:bench_name], "x"
-    assert_equality env[:bench_action_original], nil
+    menv = forge_with ["x"]
+    assert_equality menv[:bench_name_original], "x"
+    assert_equality menv[:bench_name], "x"
+    assert_equality menv[:bench_action_original], nil
   end
   
   test :forge, false, :action do
-    env = forge_with ["x:y"]
-    assert_equality env[:bench_name_original], "x"
-    assert_equality env[:bench_name], "x"
-    assert_equality env[:bench_action_original], "y"
+    menv = forge_with ["x:y"]
+    assert_equality menv[:bench_name_original], "x"
+    assert_equality menv[:bench_name], "x"
+    assert_equality menv[:bench_action_original], "y"
   end
 
 end
