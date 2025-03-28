9b60ccbac018ff2af8115984e83c604a694e50e0
Thiago Almeida <thiago@lizarb.org>
2025-03-28 18:55:49 +0000

    WebSystem RequestPanel - small fix

--- lib/web_system/subsystems/request/request_panel_test.rb
+++ lib/web_system/subsystems/request/request_panel_test.rb
@@ -16,16 +16,16 @@ class WebSystem::RequestPanelTest < Liza::PanelTest
   #
 
   test :call! do
-    env = {}
-    env["PATH_INFO"] = "/foo/bar/baz"
-    status, _headers, _body = subject.call! env
+    menv = {}
+    menv["PATH_INFO"] = "/foo/bar/baz"
+    status, _headers, _body = subject.call! menv
     assert_equality status, 404
   end
 
   test :call do
-    env = {}
-    env["PATH_INFO"] = "/foo/bar/baz"
-    status, _headers, body = subject.call env
+    menv = {}
+    menv["PATH_INFO"] = "/foo/bar/baz"
+    status, _headers, body = subject.call menv
     assert_equality status, 404
     assert_equality body, ["Client Error 404 - /foo/bar/baz"]
   end
@@ -33,20 +33,20 @@ class WebSystem::RequestPanelTest < Liza::PanelTest
   #
 
   test :_prepare do
-    env = {}
-    env["PATH_INFO"]   = "/foo/bar/baz"
-    subject._prepare env
-    assert_equality env, {
+    menv = {}
+    menv["PATH_INFO"]   = "/foo/bar/baz"
+    subject._prepare menv
+    assert_equality menv, {
       "PATH_INFO"=>"/foo/bar/baz",
       "LIZA_PATH"=>"/foo/bar/baz",
       "LIZA_FORMAT"=>"html",
       "LIZA_SEGMENTS"=>["foo", "bar", "baz"]
     }
 
-    env = {}
-    env["PATH_INFO"]   = "/"
-    subject._prepare env
-    assert_equality env, {
+    menv = {}
+    menv["PATH_INFO"]   = "/"
+    subject._prepare menv
+    assert_equality menv, {
       "PATH_INFO"=>"/",
       "LIZA_PATH"=>"/",
       "LIZA_FORMAT"=>"html",
@@ -55,19 +55,19 @@ class WebSystem::RequestPanelTest < Liza::PanelTest
   end
 
   test :find, :invalid do
-    env = {}
-    env["PATH_INFO"]   = "/foo/bar/baz"
-    request_class = subject.find env
+    menv = {}
+    menv["PATH_INFO"]   = "/foo/bar/baz"
+    request_class = subject.find menv
     assert_equality subject.routers.keys, []
     assert_equality request_class, WebSystem::NotFoundRequest
   end
 
   test :find, :valid do
-    env = {}
-    env["PATH_INFO"]   = "/"
+    menv = {}
+    menv["PATH_INFO"]   = "/"
     subject.router :simple
     assert_equality subject.routers.keys, [:simple]
-    request_class = subject.find env
+    request_class = subject.find menv
     assert_equality request_class, RootRequest
   end
 
