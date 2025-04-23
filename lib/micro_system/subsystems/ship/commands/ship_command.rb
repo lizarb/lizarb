f09a8aa97735265fdaea1e641559e9bf1480261b
Thiago Almeida <thiago@lizarb.org>
2025-04-23 23:17:33 +0000

    MicroSystem ShipCommand - call_compose

--- lib/micro_system/subsystems/ship/commands/ship_command.rb
+++ lib/micro_system/subsystems/ship/commands/ship_command.rb
@@ -2,14 +2,23 @@ class MicroSystem::ShipCommand < DevSystem::SimpleCommand
 
   # liza ship
   def call_default
-    log stick :b, system.color, "I just think Ruby is the Best for coding!"
-
     call_compose
   end
 
   # liza ship:compose
   def call_compose
     log stick :b, system.color, "I just think Ruby is the Best for coding!"
+
+    default_ship_name = Ship.panel.docked_ship_name || "default"
+    ship_name = simple_args[0] || default_ship_name
+    ship = Liza.const "#{ship_name}_ship"
+    ship.dock log_level: :higher
+  rescue Liza::ConstNotFound => e
+    log "Ship not found: #{ship_name}"
+    AppShell.consts
+    ships = Ship.descendants.select { _1.used_services.any? }
+    names = ships.map { _1.last_namespace.snakecase[0..-6] }
+    log "Available ships: #{names.join(", ")}"
   end
 
 end
