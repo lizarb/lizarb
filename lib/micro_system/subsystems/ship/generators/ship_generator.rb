ad9dc44289b9ba9ce7f6f92d2b7e3ee41cfe2bee
Thiago Almeida <thiago@lizarb.org>
2025-04-23 21:08:14 +0000

    MicroSystem ShipGenerator - call_docker

--- lib/micro_system/subsystems/ship/generators/ship_generator.rb
+++ lib/micro_system/subsystems/ship/generators/ship_generator.rb
@@ -8,21 +8,26 @@ class MicroSystem::ShipGenerator < DevSystem::ControllerGenerator
   # set_default_views "adjacent"
   # set_default_views "nested"
 
+  def arg_action_names
+    ret = super
+    ret = ["hello_world"] if ret.empty?
+    ret
+  end
 
   # liza g ship name action_1 action_2 action_3
   def call_default
-    set_default_super ""
-
-    create_controller do |unit, test|
-      unit.section name: :actions, render_key: :section_default_actions
-
-      arg_action_names.each do |action_name|
-        unit.view name: action_name, render_key: :view_default, format: :txt
-      end unless arg_views_none?
-
-      test.section name: :subject
-    end
+    call_docker
   end
 
+  def call_docker
+    set_default_super "docker"
+
+    create_controller do |unit, test|
+      unit.section name: :services, render_key: :section_docker
+
+      test.section name: :subject
+      test.section name: :services, render_key: :section_docker_test
+    end
+  end
 
 end
