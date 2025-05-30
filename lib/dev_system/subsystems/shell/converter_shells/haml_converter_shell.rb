0b75938f54378bddf9e11885319d0aeb24916564
Thiago Almeida <thiago@lizarb.org>
2025-05-30 04:18:57 +0000

    DevSystem HamlConverterShell - add_source

--- lib/dev_system/subsystems/shell/converter_shells/haml_converter_shell.rb
+++ lib/dev_system/subsystems/shell/converter_shells/haml_converter_shell.rb
@@ -21,9 +21,21 @@ class DevSystem::HamlConverterShell < DevSystem::ConverterShell
     raise if menv[:raise_errors]
     log stick :light_white, :red, :b, "#{e.class}: #{e.message}"
     menv[:error] = e
-    menv[:convert_out] = menv[:convert_in]
+    menv[:convert_out] = "#{e.class}: #{e.message}"
   ensure
+    add_source(menv) if $coding or menv[:add_source]
     nil
+  end
+
+
+  def self.add_source(menv)
+    menv[:convert_out] += <<STRING
+
+<!--
+SOURCE:
+#{menv[:convert_in].gsub("-->", "--/>")}
+-->
+STRING
   end
 
 end
