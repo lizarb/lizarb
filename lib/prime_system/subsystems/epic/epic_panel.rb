32332b598d5a913c728b63fa00c88e16218c4635
Thiago Almeida <thiago@lizarb.org>
2025-06-18 20:43:12 +0000

    PrimeSystem EpicPanel - small fix

--- lib/prime_system/subsystems/epic/epic_panel.rb
+++ lib/prime_system/subsystems/epic/epic_panel.rb
@@ -6,16 +6,16 @@ class PrimeSystem::EpicPanel < Liza::Panel
     "epic not found: #{args[0].inspect}"
   end
 
-  def call(other_env)
+  def call(other_menv)
     log "not implemented"
   #   log "forging"
-  #   env = { other_env: }
+  #   menv = { other_menv: }
 
   #   log "finding"
-  #   k = Liza.const "#{other_env[:epic]}_epic"
+  #   k = Liza.const "#{other_menv[:epic]}_epic"
     
   #   log "forwarding"
-  #   k.call env
+  #   k.call menv
   # rescue Liza::ConstNotFound
   #   raise_error :not_found, string
   end
