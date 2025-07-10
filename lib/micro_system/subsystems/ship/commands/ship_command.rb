874f6f122fabc96c56b2bd3684133f26c3c20ac5
Thiago Almeida <thiago@lizarb.org>
2025-07-10 00:16:49 +0000

    MicroSystem ShipCommand - small fix

--- lib/micro_system/subsystems/ship/commands/ship_command.rb
+++ lib/micro_system/subsystems/ship/commands/ship_command.rb
@@ -7,20 +7,18 @@ class MicroSystem::ShipCommand < DevSystem::SimpleCommand
 
   # liza ship:up
   def call_up
-    log stick :b, system.color, "I just think Ruby is the Best for coding!"
     return if ship.nil?
 
     filename = "docker-compose.#{ship.token}.yml"
-    log "Docking #{ship} to #{filename}"
+    log stick :b, cl.system.color, "Docking #{ship} to #{filename}"
     ship.up log_level: :higher, filename:
   end
 
   # liza ship:compose
   def call_compose
-    log stick :b, system.color, "I just think Ruby is the Best for coding!"
     return if ship.nil?
 
-    log "Docking #{ship} to docker-compose.yml"
+    log stick :b, cl.system.color, "Docking #{ship} to docker-compose.yml"
     ship.dock log_level: :higher
   end
 
