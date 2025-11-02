aed131eeb1a8cc8932597e2028e85a20dd493dcf
Thiago Almeida <thiago@lizarb.org>
2025-11-02 15:42:48 -0600

    DeskSystem GlimmerCommand - small fix

--- lib/desk_system/commands/glimmer_command.rb
+++ lib/desk_system/commands/glimmer_command.rb
@@ -9,14 +9,14 @@ class DeskSystem::GlimmerCommand < DevSystem::SimpleCommand
   # liza glimmer:examples
 
   def call_examples
-    log stick :b, system.color, "Glimmer is a free Ruby Desktop Development Cross-Platform Native GUI Library!"
+    log stick :b, cl.system.color, "Glimmer is a free Ruby Desktop Development Cross-Platform Native GUI Library!"
     log [
       (stick :darkest_white, "Learn more at "),
-      (stick :b, system.color, "https://github.com/AndyObtiva/glimmer-dsl-libui")
+      (stick :b, cl.system.color, "https://github.com/AndyObtiva/glimmer-dsl-libui")
     ]
     
     puts
-    log "args = #{env[:args].inspect}"
+    log "args = #{menv[:args].inspect}"
     # require "glimmer-dsl-libui"
     require "csv"
     require "examples/meta_example"
