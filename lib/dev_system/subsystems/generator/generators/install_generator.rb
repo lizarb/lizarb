871e34c228cca2801ff0e20deb0a54f785647d56
Thiago Almeida <thiago@lizarb.org>
2026-01-06 11:22:23 -0600

    DevSystem InstallGenerator - removed

--- lib/dev_system/subsystems/generator/generators/install_generator.rb
+++
@@ -1,9 +0,0 @@
-class DevSystem::InstallGenerator < DevSystem::SimpleGenerator
-  
-  # liza g install
-  
-  def call_default
-    log stick :red, "Not implemented yet"
-  end
-
-end
--- lib/dev_system/subsystems/generator/generators/install_generator_test.rb
+++
@@ -1,8 +0,0 @@
-class DevSystem::InstallGeneratorTest < DevSystem::SimpleGeneratorTest
-  
-  test :subject_class, :subject do
-    assert_equality DevSystem::InstallGenerator, subject_class
-    assert_equality DevSystem::InstallGenerator, subject.class
-  end
-  
-end
