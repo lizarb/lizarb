1f60425e4c804cf689e5e85b45b7168798322f90
Thiago Almeida <thiago@lizarb.org>
2026-01-18 22:15:25 -0600

    DevSystem GenerateCommand - small fix

--- lib/dev_system/subsystems/generator/commands/generate_command.rb
+++ lib/dev_system/subsystems/generator/commands/generate_command.rb
@@ -6,13 +6,6 @@ class DevSystem::GenerateCommand < DevSystem::SimpleCommand
   shortcut :m, :move
   shortcut :o, :override
 
-  def before
-    super
-    log "simple_args     #{ simple_args }"
-    log "simple_booleans #{ simple_booleans }"
-    log "simple_strings  #{ simple_strings }"
-  end
-
   # liza generate
   def call_default
     DevBox.generate menv
